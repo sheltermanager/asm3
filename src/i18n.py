495f2b8c58825d2ff111e5134de004b8ae8436d4
Robin Rawson-Tetley <robin@grunty.robsdomain>
2015-03-27 07:38:05 +0000

    stable deploy

--- src/i18n.py
+++ src/i18n.py
@@ -4,8 +4,8 @@ import datetime
 import json
 import time
 
-VERSION = "36u [Thu 26 Mar 20:57:47 GMT 2015]"
-BUILD = "03262057"
+VERSION = "36u [Fri 27 Mar 07:34:33 GMT 2015]"
+BUILD = "03270734"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
