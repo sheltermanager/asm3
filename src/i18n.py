b3fe0ec39fb86744ee58e25c1f934c7baf716ca4
Robin Rawson-Tetley <robin@grunty.robsdomain>
2015-04-12 16:01:50 +0100

    forced redeployment

--- src/i18n.py
+++ src/i18n.py
@@ -4,8 +4,8 @@ import datetime
 import json
 import time
 
-VERSION = "37u [Sun 12 Apr 15:58:14 BST 2015]"
-BUILD = "04121558"
+VERSION = "37u [Sun 12 Apr 16:00:41 BST 2015]"
+BUILD = "04121600"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
