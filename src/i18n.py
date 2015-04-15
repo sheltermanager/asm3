cb0596a4dc11adac34b7513fcd662fbbc66ba52c
Robin Rawson-Tetley <robin@slinky.robsdomain>
2015-04-15 08:43:43 +0100

    layout main screen with responsive columns #158

--- src/i18n.py
+++ src/i18n.py
@@ -4,8 +4,8 @@ import datetime
 import json
 import time
 
-VERSION = "37u [Wed 15 Apr 08:15:34 BST 2015]"
-BUILD = "04150815"
+VERSION = "37u [Wed 15 Apr 08:43:37 BST 2015]"
+BUILD = "04150843"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
