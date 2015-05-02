51da17b1095c0a81a1f6adc4c51caf4b4179b5e4
Robin Rawson-Tetley <robin@grunty.robsdomain>
2015-05-02 10:44:30 +0100

    remove cruft

--- src/i18n.py
+++ src/i18n.py
@@ -4,8 +4,8 @@ import datetime
 import json
 import time
 
-VERSION = "37u [Sat  2 May 10:42:28 BST 2015]"
-BUILD = "05021042"
+VERSION = "37u [Sat  2 May 10:44:10 BST 2015]"
+BUILD = "05021044"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
