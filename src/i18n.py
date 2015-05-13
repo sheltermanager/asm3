78b303a83bf7ae10ec380b4a0269d6e07643b770
Robin Rawson-Tetley <robin@grunty.robsdomain>
2015-05-13 18:45:02 +0100

    use BR for adoptapet and rescuegroups

--- src/i18n.py
+++ src/i18n.py
@@ -4,8 +4,8 @@ import datetime
 import json
 import time
 
-VERSION = "37u [Wed 13 May 18:04:07 BST 2015]"
-BUILD = "05131804"
+VERSION = "37u [Wed 13 May 18:45:00 BST 2015]"
+BUILD = "05131845"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
