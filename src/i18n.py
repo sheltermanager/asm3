275d67f2c1ac62360cf15b86b8b034972e6c3cb0
Robin Rawson-Tetley <robin@slinky.robsdomain>
2015-05-25 08:46:04 +0100

    css tweak for max-width of media element

--- src/i18n.py
+++ src/i18n.py
@@ -4,8 +4,8 @@ import datetime
 import json
 import time
 
-VERSION = "37u [Mon 25 May 08:41:49 BST 2015]"
-BUILD = "05250841"
+VERSION = "37u [Mon 25 May 08:45:59 BST 2015]"
+BUILD = "05250845"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
