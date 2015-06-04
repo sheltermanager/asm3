50c8f71bccd18131ccd5160dae0447ead8159d48
Robin Rawson-Tetley <robin@slinky.robsdomain>
2015-06-04 19:01:23 +0100

    fix new map

--- src/i18n.py
+++ src/i18n.py
@@ -4,8 +4,8 @@ import datetime
 import json
 import time
 
-VERSION = "37u [Thu  4 Jun 18:32:11 BST 2015]"
-BUILD = "06041832"
+VERSION = "37u [Thu  4 Jun 18:59:53 BST 2015]"
+BUILD = "06041859"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
