e86d3b62ad0175f18ac2b818d5faa2282aa8f511
Robin Rawson-Tetley <robin@slinky.robsdomain>
2015-06-05 09:22:31 +0100

    fix default user creation

--- src/i18n.py
+++ src/i18n.py
@@ -4,8 +4,8 @@ import datetime
 import json
 import time
 
-VERSION = "37u [Fri  5 Jun 09:17:59 BST 2015]"
-BUILD = "06050917"
+VERSION = "37u [Fri  5 Jun 09:22:24 BST 2015]"
+BUILD = "06050922"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
