240794f4e929c3d365be17c32fd81e9c89a100e3
Robin Rawson-Tetley <robin@slinky.robsdomain>
2015-06-06 09:24:41 +0100

    06/06/15 Complete set of adoptapet colour mappings

--- src/i18n.py
+++ src/i18n.py
@@ -4,8 +4,8 @@ import datetime
 import json
 import time
 
-VERSION = "37u [Sat  6 Jun 08:48:27 BST 2015]"
-BUILD = "06060848"
+VERSION = "37u [Sat  6 Jun 09:24:27 BST 2015]"
+BUILD = "06060924"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
