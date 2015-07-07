ee3fc7fd7b45881b1b2b2c8931dc1c4867ec369f
Robin Rawson-Tetley <robin@grunty.robsdomain>
2015-07-07 15:54:48 +0100

    cast before doing replace on address hash in case of blanks calculating geocodes

--- src/i18n.py
+++ src/i18n.py
@@ -4,8 +4,8 @@ import datetime
 import json
 import time
 
-VERSION = "37u [Mon  6 Jul 21:26:10 BST 2015]"
-BUILD = "07062126"
+VERSION = "37u [Tue  7 Jul 15:54:24 BST 2015]"
+BUILD = "07071554"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
