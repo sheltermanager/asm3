4ca9d96fc8a14ab2ce28945ce90727d5ec40c82a
Robin Rawson-Tetley <robin@slinky.robsdomain>
2015-07-16 10:00:30 +0100

    add med, vacc and tests between two dates version

--- src/i18n.py
+++ src/i18n.py
@@ -4,8 +4,8 @@ import datetime
 import json
 import time
 
-VERSION = "37u [Thu 16 Jul 09:28:12 BST 2015]"
-BUILD = "07160928"
+VERSION = "37u [Thu 16 Jul 09:47:00 BST 2015]"
+BUILD = "07160947"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
