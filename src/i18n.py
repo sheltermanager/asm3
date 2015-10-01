c0b8e4eb08d8fed435988091fc9e24747636d114
Robin Rawson-Tetley <robin@grunty.robsdomain>
2015-10-01 12:02:25 +0100

    correct bad column alignments, account for gift aid and deposit account override

--- src/i18n.py
+++ src/i18n.py
@@ -4,8 +4,8 @@ import datetime
 import json
 import time
 
-VERSION = "37u [Thu  1 Oct 11:54:39 BST 2015]"
-BUILD = "10011154"
+VERSION = "37u [Thu  1 Oct 12:00:32 BST 2015]"
+BUILD = "10011200"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
