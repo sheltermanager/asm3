09d13b565056c9fa9e0f28260f7af049808550c5
Robin Rawson-Tetley <robin@grunty.robsdomain>
2015-10-12 17:30:50 +0100

    use zipalign for play store

--- cordova/asm/signed_apk/make_signed.sh
+++ cordova/asm/signed_apk/make_signed.sh
@@ -4,3 +4,5 @@ cordova build android --release
 cd signed_apk
 cp ../platforms/android/build/outputs/apk/android-release-unsigned.apk asm.apk
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore asm-release-key.keystore asm.apk asm
+~/usr/android-sdk-linux/build-tools/23.0.1/zipalign -f 4 asm.apk sheltermanager.apk
+rm asm.apk
