04777cbf894bf466dffc0b6c8358e11266d6ab44
Robin Rawson-Tetley <robin@grunty.robsdomain>
2015-10-16 19:08:23 +0100

    update translations for non-unique french account code

--- src/i18n.py
+++ src/i18n.py
@@ -4,8 +4,8 @@ import datetime
 import json
 import time
 
-VERSION = "37u [Fri 16 Oct 19:05:08 BST 2015]"
-BUILD = "10161905"
+VERSION = "37u [Fri 16 Oct 19:08:20 BST 2015]"
+BUILD = "10161908"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
