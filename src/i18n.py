8a292b2de0358ad9f966011303cdc734a6da3ef3
Robin Rawson-Tetley <robin@grunty.robsdomain>
2015-10-30 13:28:05 +0000

    fix bug in littlebox template for new installs

--- src/i18n.py
+++ src/i18n.py
@@ -4,8 +4,8 @@ import datetime
 import json
 import time
 
-VERSION = "37u [Fri 30 Oct 12:36:08 GMT 2015]"
-BUILD = "10301236"
+VERSION = "37u [Fri 30 Oct 13:26:59 GMT 2015]"
+BUILD = "10301326"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
