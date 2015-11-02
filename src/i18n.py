e6a7174b7c9baa8e0aed6463d8e7736062832956
Robin Rawson-Tetley <robin@development.robsdomain>
2015-11-02 08:34:14 +0000

    fix spinner

--- src/i18n.py
+++ src/i18n.py
@@ -4,8 +4,8 @@ import datetime
 import json
 import time
 
-VERSION = "37u [Mon  2 Nov 08:22:12 GMT 2015]"
-BUILD = "11020822"
+VERSION = "37u [Mon  2 Nov 08:28:30 GMT 2015]"
+BUILD = "11020828"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
