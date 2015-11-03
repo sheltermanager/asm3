44a0f14ae493ea6513f7e4ac0d4d7c7e8b3cfac4
Robin Rawson-Tetley <robin@grunty.robsdomain>
2015-11-03 11:48:12 +0000

    remove experimental svgs

--- src/i18n.py
+++ src/i18n.py
@@ -4,8 +4,8 @@ import datetime
 import json
 import time
 
-VERSION = "37u [Tue  3 Nov 11:46:54 GMT 2015]"
-BUILD = "11031146"
+VERSION = "37u [Tue  3 Nov 11:48:09 GMT 2015]"
+BUILD = "11031148"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
