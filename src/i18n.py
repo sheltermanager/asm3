f123e2585e4a7379fac1c9c771358a389ba23909
Robin Rawson-Tetley <robin@development.robsdomain>
2015-11-04 08:14:23 +0000

    04/11/15 Include movement attached to selected donation on receipt document, 04/11/15 Include last movement tags for person document

--- src/i18n.py
+++ src/i18n.py
@@ -4,8 +4,8 @@ import datetime
 import json
 import time
 
-VERSION = "37u [Tue  3 Nov 19:16:04 GMT 2015]"
-BUILD = "11031916"
+VERSION = "37u [Wed  4 Nov 08:14:15 GMT 2015]"
+BUILD = "11040814"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
