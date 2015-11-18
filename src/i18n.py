37f3127aa4c20e87596631eb5ab6438ec85202e4
Robin Rawson-Tetley <robin@grunty.robsdomain>
2015-11-18 11:46:38 +0000

    18/11/15 Make disabled fields more obvious

--- src/i18n.py
+++ src/i18n.py
@@ -4,8 +4,8 @@ import datetime
 import json
 import time
 
-VERSION = "37u [Wed 18 Nov 11:26:01 GMT 2015]"
-BUILD = "11181126"
+VERSION = "37u [Wed 18 Nov 11:46:23 GMT 2015]"
+BUILD = "11181146"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
