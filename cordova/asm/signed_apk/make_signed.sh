8869da0c4f79fb7507906eac6d95c5ce35146ca5
Robin Rawson-Tetley <robin@grunty.robsdomain>
2015-12-18 17:10:03 +0000

    moved into separate project

--- cordova/asm/signed_apk/make_signed.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd ..
-cordova build android --release
-cd signed_apk
-cp ../platforms/android/build/outputs/apk/android-release-unsigned.apk asm.apk
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore asm-release-key.keystore asm.apk asm
-~/usr/android-sdk-linux/build-tools/23.0.1/zipalign -f 4 asm.apk sheltermanager.apk
-rm asm.apk
