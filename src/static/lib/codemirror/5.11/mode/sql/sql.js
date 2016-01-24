40bdaab090d202221616fc6461b1d2b41998b122
Robin Rawson-Tetley <robin@grunty.robsdomain>
2016-01-24 18:44:27 +0000

    add missing join keywords to generic list

--- src/static/lib/codemirror/5.11/mode/sql/sql.js
+++ src/static/lib/codemirror/5.11/mode/sql/sql.js
@@ -270,7 +270,7 @@ CodeMirror.defineMode("sql", function(config, parserConfig) {
   CodeMirror.defineMIME("text/x-sql", {
     name: "sql",
     keywords: set(sqlKeywords + "begin"),
-    builtin: set("bool boolean bit blob enum long longblob longtext medium mediumblob mediumint mediumtext time timestamp tinyblob tinyint tinytext text bigint int int1 int2 int3 int4 int8 integer float float4 float8 double char varbinary varchar varcharacter precision real date datetime year unsigned signed decimal numeric"),
+    builtin: set("bool boolean bit blob enum inner left long longblob longtext medium mediumblob mediumint mediumtext outer time timestamp tinyblob tinyint tinytext text bigint int int1 int2 int3 int4 int8 integer float float4 float8 double char varbinary varchar varcharacter precision real right date datetime year unsigned signed decimal numeric"),
     atoms: set("false true null unknown"),
     operatorChars: /^[*+\-%<>!=]/,
     dateSQL: set("date time timestamp"),
