26cff6e83af8b0c602322e7efa3d060a92dba6ca
Robin Rawson-Tetley <robin@grunty.robsdomain>
2016-02-08 16:08:48 +0000

    non-shelter animals shouldn't have onshelter flags available

--- src/i18n.py
+++ src/i18n.py
@@ -4,8 +4,8 @@ import datetime
 import json
 import time
 
-VERSION = "39u [Mon  8 Feb 08:54:57 GMT 2016]"
-BUILD = "02080854"
+VERSION = "39u [Mon  8 Feb 16:07:03 GMT 2016]"
+BUILD = "02081607"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
