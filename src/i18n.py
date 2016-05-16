e71aee8339427185cafa45133403b320501ef63f
Robin Rawson-Tetley <robin@grunty.robsdomain>
2016-05-16 12:27:53 +0100

    fix login screen properly

--- src/i18n.py
+++ src/i18n.py
@@ -4,8 +4,8 @@ import datetime
 import json
 import time
 
-VERSION = "39u [Mon 16 May 12:23:41 BST 2016]"
-BUILD = "05161223"
+VERSION = "39u [Mon 16 May 12:25:58 BST 2016]"
+BUILD = "05161225"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
