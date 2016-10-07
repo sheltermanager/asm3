a6301d0f9a4d361ca5afce5a170d86537f4bc330
Robin Rawson-Tetley <robin@rob-thinkpad.robsdomain>
2016-10-07 09:56:54 +0100

    39 release

--- src/i18n.py
+++ src/i18n.py
@@ -4,8 +4,8 @@ import datetime
 import json
 import time
 
-VERSION = "39u [Thu  6 Oct 16:33:52 BST 2016]"
-BUILD = "10061633"
+VERSION = "39 [Fri  7 Oct 09:45:57 BST 2016]"
+BUILD = "10070945"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
