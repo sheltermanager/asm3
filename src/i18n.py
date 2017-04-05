fec54d657a893be2eae893f6e30a5fd78537ad50
Robin Rawson-Tetley <robin@curvy.robsdomain>
2017-04-05 10:11:58 +0100

    show description exclusions correctly

--- src/i18n.py
+++ src/i18n.py
@@ -4,8 +4,8 @@ import datetime
 import json
 import time
 
-VERSION = "40u [Wed  5 Apr 10:09:30 BST 2017]"
-BUILD = "04051009"
+VERSION = "40u [Wed  5 Apr 10:11:31 BST 2017]"
+BUILD = "04051011"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
