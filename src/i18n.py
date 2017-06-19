6a05cefc954082333945e74ec122bfb42de90ebe
Robin Rawson-Tetley <robin@rob-thinkpad.robsdomain>
2017-06-19 09:52:37 +0100

    fix real_locale mapping for i18nstrings

--- src/i18n.py
+++ src/i18n.py
@@ -7,8 +7,8 @@ import time
 # flake8: noqa - we have a lot of locales and this is convenient
 from locales import *
 
-VERSION = "40u [Sat 17 Jun 21:57:56 BST 2017]"
-BUILD = "06172157"
+VERSION = "40u [Mon 19 Jun 09:50:53 BST 2017]"
+BUILD = "06190950"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
@@ -598,7 +598,7 @@ def i18nstringsjs(l):
     """
     langs = "{}"
     try:
-        lang = globals()["locale_" + l]
+        lang = globals()["locale_" + real_locale(l)]
         langs = json.dumps(lang.val)
     except:
         pass
