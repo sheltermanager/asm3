7ba35cc0504fa5eb72ac792b4eac74fd04a0137a
Robin Rawson-Tetley <robin@curvy.robsdomain>
2017-09-18 20:22:49 +0100

    Allow register on intake to AVID users where register overseas is on

--- src/i18n.py
+++ src/i18n.py
@@ -7,8 +7,8 @@ import time
 # flake8: noqa - we have a lot of locales and this is convenient
 from locales import *
 
-VERSION = "40u [Mon 18 Sep 16:47:47 BST 2017]"
-BUILD = "09181647"
+VERSION = "40u [Mon 18 Sep 20:20:29 BST 2017]"
+BUILD = "09182020"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
