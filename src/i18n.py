3a827b08861c3f242cd0b936d01adcc324983e82
Robin Rawson-Tetley <robin@curvy.robsdomain>
2017-10-28 15:37:59 +0100

    save checkbox

--- src/i18n.py
+++ src/i18n.py
@@ -7,8 +7,8 @@ import time
 # flake8: noqa - we have a lot of locales and this is convenient
 from locales import *
 
-VERSION = "40u [Sat 28 Oct 15:35:03 BST 2017]"
-BUILD = "10281535"
+VERSION = "40u [Sat 28 Oct 15:37:17 BST 2017]"
+BUILD = "10281537"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
