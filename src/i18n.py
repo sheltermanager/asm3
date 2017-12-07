2a30e4e6fb1b5d57ce0056c21c8b262d52a4de98
Robin Rawson-Tetley <robin@curvy.robsdomain>
2017-12-07 12:43:16 +0000

    Auto fix multiple website/docphoto being set

--- src/i18n.py
+++ src/i18n.py
@@ -7,8 +7,8 @@ import time
 # flake8: noqa - we have a lot of locales and this is convenient
 from locales import *
 
-VERSION = "40u [Thu  7 Dec 10:37:28 GMT 2017]"
-BUILD = "12071037"
+VERSION = "40u [Thu  7 Dec 12:41:33 GMT 2017]"
+BUILD = "12071241"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
