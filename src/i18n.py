8d850b331d0da0669757c40f726e763be2dffb7a
Robin Rawson-Tetley <robin@curvy.robsdomain>
2018-01-22 15:04:51 +0000

    22/01/18 Add new  SmartTag prefix 900141

--- src/i18n.py
+++ src/i18n.py
@@ -7,8 +7,8 @@ import time
 # flake8: noqa - we have a lot of locales and this is convenient
 from locales import *
 
-VERSION = "40u [Fri 19 Jan 17:41:42 GMT 2018]"
-BUILD = "01191741"
+VERSION = "40u [Mon 22 Jan 15:01:00 GMT 2018]"
+BUILD = "01221501"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
--- src/lookups.py
+++ src/lookups.py
@@ -234,6 +234,7 @@ MICROCHIP_MANUFACTURERS = [
     { "length": 15, "regex": r"^900138", "name": "ID-Ology", "locales": "" },
     { "length": 15, "regex": r"^900164", "name": "Save This Life", "locales": "" },
     { "length": 15, "regex": r"^900182", "name": "Petlog", "locales": "" },
+    { "length": 15, "regex": r"^900141", "name": "SmartTag", "locales": "" },
     { "length": 15, "regex": r"^900", "name": "BCDS", "locales": "" },
     { "length": 15, "regex": r"^911002", "name": "911PetChip", "locales": "" },
     { "length": 15, "regex": r"^939", "name": "M4S ID", "locales": "" },
