eefa0138d79edd0aab681e0684384cc5bcccd00b
Robin Rawson-Tetley <robin@curvy.robsdomain>
2018-02-01 15:40:38 +0000

    fix rabies number too

--- import/animalshelternet_mysql.py
+++ import/animalshelternet_mysql.py
@@ -22,7 +22,8 @@ FIX_FIELD_LENGTHS = True
 if FIX_FIELD_LENGTHS:
     db.query("ALTER TABLE animals " \
         "MODIFY Name VARCHAR(255), " \
-        "MODIFY MicrochipID VARCHAR(255)")
+        "MODIFY MicrochipID VARCHAR(255), " \
+        "MODIFY RabiesNum VARCHAR(255)")
 
 def gettypeletter(aid):
     tmap = {
