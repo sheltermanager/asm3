cc6b0c13c4f1964dc56f047936d5ea5ae81c27b0
Robin Rawson-Tetley <robin@curvy.robsdomain>
2018-02-07 16:27:18 +0000

    some helpful timezone abbreviations

--- src/i18n.py
+++ src/i18n.py
@@ -7,8 +7,8 @@ import time
 # flake8: noqa - we have a lot of locales and this is convenient
 from locales import *
 
-VERSION = "41u [Wed  7 Feb 09:14:47 GMT 2018]"
-BUILD = "02070914"
+VERSION = "41u [Wed  7 Feb 16:22:13 GMT 2018]"
+BUILD = "02071622"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
