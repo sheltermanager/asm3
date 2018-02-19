70ab9a72871eadf8a06b040616338f10c9f9f304
Robin Rawson-Tetley <robin@rob-thinkpad.robsdomain>
2018-02-19 19:17:28 +0000

    enable 34103

--- src/i18n.py
+++ src/i18n.py
@@ -7,8 +7,8 @@ import time
 # flake8: noqa - we have a lot of locales and this is convenient
 from locales import *
 
-VERSION = "41u [Mon 19 Feb 11:37:54 GMT 2018]"
-BUILD = "02191137"
+VERSION = "41u [Mon 19 Feb 19:11:41 GMT 2018]"
+BUILD = "02191911"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
