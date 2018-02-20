d6ba98c31745420388aa6b0f72ce0751beb850e5
Robin Rawson-Tetley <robin@curvy.robsdomain>
2018-02-20 11:04:32 +0000

    Rewrite S3Storage to use boto3 #375

--- src/i18n.py
+++ src/i18n.py
@@ -7,8 +7,8 @@ import time
 # flake8: noqa - we have a lot of locales and this is convenient
 from locales import *
 
-VERSION = "41u [Mon 19 Feb 11:37:54 GMT 2018]"
-BUILD = "02191137"
+VERSION = "41u [Tue 20 Feb 11:02:20 GMT 2018]"
+BUILD = "02201102"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
