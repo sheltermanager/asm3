21c3598d416fd7464a2512411100b10a39c8c067
Robin Rawson-Tetley <robin@curvy.robsdomain>
2018-03-06 08:48:22 +0000

    use sitedef for smcom login link

--- src/i18n.py
+++ src/i18n.py
@@ -7,8 +7,8 @@ import time
 # flake8: noqa - we have a lot of locales and this is convenient
 from locales import *
 
-VERSION = "41u [Mon  5 Mar 10:22:59 GMT 2018]"
-BUILD = "03051022"
+VERSION = "41u [Tue  6 Mar 08:47:37 GMT 2018]"
+BUILD = "03060847"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
--- src/static/js/login.js
+++ src/static/js/login.js
@@ -132,7 +132,7 @@ $(function() {
                     else if (String(data).indexOf("WRONGSERVER") != -1) {
                         // This is smcom specific - if the database is not on this
                         // server, go back to the main login screen to prompt for an account
-                        window.location = "https://sheltermanager.com/service/asmlogin";
+                        window.location = controller.smcomloginlink;
                     }
                     else {
                         // We have a successful login!
