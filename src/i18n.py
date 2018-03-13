39ac3edf3a30c760a3cf44c3422752063b160120
Robin Rawson-Tetley <robin@curvy.robsdomain>
2018-03-13 22:04:49 +0000

    use some longer browser caches on service calls

--- src/i18n.py
+++ src/i18n.py
@@ -7,8 +7,8 @@ import time
 # flake8: noqa - we have a lot of locales and this is convenient
 from locales import *
 
-VERSION = "41u [Tue 13 Mar 21:55:43 GMT 2018]"
-BUILD = "03132155"
+VERSION = "41u [Tue 13 Mar 22:01:20 GMT 2018]"
+BUILD = "03132201"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
