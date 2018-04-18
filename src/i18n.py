1e925e606190baa77f4c2cf89296ac5e2f8a415f
Robin Rawson-Tetley <robin@curvy.robsdomain>
2018-04-18 11:08:50 +0100

    refactor petlink permanent failures so the code is more readable and it's easier to add new ones

--- src/i18n.py
+++ src/i18n.py
@@ -7,8 +7,8 @@ import time
 # flake8: noqa - we have a lot of locales and this is convenient
 from locales import *
 
-VERSION = "41u [Wed 18 Apr 10:42:54 BST 2018]"
-BUILD = "04181042"
+VERSION = "41u [Wed 18 Apr 10:58:33 BST 2018]"
+BUILD = "04181058"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
