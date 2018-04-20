d7b22ecdb2748f8dec8cb3a33aa65b3c9ed79823
Robin Rawson-Tetley <robin@curvy.robsdomain>
2018-04-20 15:39:19 +0100

    Include species column in medical books #367

--- src/i18n.py
+++ src/i18n.py
@@ -7,8 +7,8 @@ import time
 # flake8: noqa - we have a lot of locales and this is convenient
 from locales import *
 
-VERSION = "41u [Thu 19 Apr 08:46:13 BST 2018]"
-BUILD = "04190846"
+VERSION = "41u [Fri 20 Apr 15:34:40 BST 2018]"
+BUILD = "04201534"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
