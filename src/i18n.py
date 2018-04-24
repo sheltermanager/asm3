5f51252935c809465b02d56a58eeb883d7ce2014
Robin Rawson-Tetley <robin@curvy.robsdomain>
2018-04-24 16:00:12 +0100

    add copy to ResultRow to wrap the resulting dict.copy

--- src/i18n.py
+++ src/i18n.py
@@ -7,8 +7,8 @@ import time
 # flake8: noqa - we have a lot of locales and this is convenient
 from locales import *
 
-VERSION = "41u [Tue 24 Apr 13:21:24 BST 2018]"
-BUILD = "04241321"
+VERSION = "41u [Tue 24 Apr 15:55:51 BST 2018]"
+BUILD = "04241555"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
