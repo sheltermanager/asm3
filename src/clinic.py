dbec7e2df16f74ed6ec30620fe89df9a013a95c2
Robin Rawson-Tetley <robin@curvy.robsdomain>
2018-04-27 13:06:54 +0100

    Clinic backend and unit tests #9

---
+++ src/clinic.py
@@ -0,0 +1,135 @@
+#!/usr/bin/python
+
+import i18n
+import utils
+
+SCHEDULED = 0
+INVOICE_ONLY = 1
+NOT_ARRIVED = 2
+WAITING = 3
+WITH_VET = 4
+COMPLETE = 5
+CANCELLED = 6
+
+ASCENDING = 0
+DESCENDING = 1
+
+def get_clinic_appointment_query(dbo):
+    return "SELECT ca.*, o.OwnerTitle, o.OwnerInitials, o.OwnerSurname, o.OwnerForenames, o.OwnerName, " \
+        "o.OwnerAddress, o.OwnerTown, o.OwnerCounty, o.OwnerPostcode, o.HomeTelephone, o.WorkTelephone, o.MobileTelephone, " \
+        "cs.Status AS ClinicStatusName, " \
+        "a.ShelterCode, a.ShortCode, a.AnimalAge, a.AgeGroup, a.AnimalName, a.BreedName, a.Neutered, a.DeceasedDate, a.HasActiveReserve, " \
+        "a.HasTrialAdoption, a.IsHold, a.IsQuarantine, a.HoldUntilDate, a.CrueltyCase, a.NonShelterAnimal, " \
+        "a.ActiveMovementType, a.Archived, a.IsNotAvailableForAdoption, " \
+        "a.CombiTestResult, a.FLVResult, a.HeartwormTestResult, " \
+        "ma.MediaName AS WebsiteMediaName, ma.Date AS WebsiteMediaDate, " \
+        "sx.Sex, s.SpeciesName " \
+        "FROM clinicappointment ca " \
+        "LEFT OUTER JOIN lksclinicstatus cs ON cs.ID = ca.Status " \
+        "LEFT OUTER JOIN animal a ON ca.AnimalID = a.ID " \
+        "LEFT OUTER JOIN media ma ON ma.LinkID = a.ID AND ma.LinkTypeID = 0 AND ma.WebsitePhoto = 1 " \
+        "LEFT OUTER JOIN species s ON a.SpeciesID = s.ID " \
+        "LEFT OUTER JOIN lksex sx ON sx.ID = a.Sex " \
+        "LEFT OUTER JOIN owner o ON ca.OwnerID = o.ID "
+
+def get_clinic_invoice_query(dbo):
+    return "SELECT ci.* " \
+        "FROM clinicinvoiceitem ci "
+
+def get_appointments_today(dbo, sort = DESCENDING):
+    """
+    Gets all appointments that are due today
+    """
+    order = "ca.DateTime"
+    if sort == DESCENDING: order += " DESC"
+    sql = "%s WHERE ca.DateTime >= ? AND ca.DateTime <= ? ORDER BY %s" % (get_clinic_appointment_query(dbo), order)
+    return dbo.query(sql, [ dbo.today(), dbo.today(settime="23:59:59") ])
+
+def get_invoice_items(dbo, appointmentid):
+    """
+    Gets all invoice items for an appointment
+    """
+    return dbo.query(get_clinic_invoice_query(dbo) + " WHERE ClinicAppointmentID = ? ORDER BY ID", [appointmentid])
+
+def insert_appointment_from_form(dbo, username, post):
+    """
+    Creates a clinic appointment from posted form data
+    """
+    l = dbo.locale
+    if post.datetime("date", "time") is None:
+        raise utils.ASMValidationError(i18n._("Appointment date must be a valid date", l))
+
+    return dbo.insert("clinicappointment", {
+        "AnimalID":             post.integer("animal"),
+        "OwnerID":              post.integer("person"),
+        "DateTime":             post.datetime("date", "time"),
+        "Status":               post.integer("status"),
+        "ArrivedDateTime":      post.datetime("arriveddate", "arrivedtime"),
+        "WithVetDateTime":      post.datetime("withvetdate", "withvettime"),
+        "CompletedDateTime":    post.datetime("completedate", "completetime"),
+        "ReasonForAppointment": post["reason"],
+        "Comments":             post["comments"],
+        "Amount":               post.integer("amount"),
+        "IsVAT":                post.integer("isvat"),
+        "VATRate":              post.floating("vatrate"),
+        "VATAmount":            post.integer("vatamount")
+    }, username)
+
+def update_appointment_from_form(dbo, username, post):
+    """
+    Updates an appointment from form data
+    """
+    l = dbo.locale
+    if post.datetime("date", "time") is None:
+        raise utils.ASMValidationError(i18n._("Appointment date must be a valid date", l))
+
+    dbo.update("clinicappointment", post.integer("appointmentid"), {
+        "AnimalID":             post.integer("animal"),
+        "OwnerID":              post.integer("person"),
+        "DateTime":             post.datetime("date", "time"),
+        "Status":               post.integer("status"),
+        "ArrivedDateTime":      post.datetime("arriveddate", "arrivedtime"),
+        "WithVetDateTime":      post.datetime("withvetdate", "withvettime"),
+        "CompletedDateTime":    post.datetime("completedate", "completetime"),
+        "ReasonForAppointment": post["reason"],
+        "Comments":             post["comments"],
+        "Amount":               post.integer("amount"),
+        "IsVAT":                post.integer("isvat"),
+        "VATRate":              post.floating("vatrate"),
+        "VATAmount":            post.integer("vatamount")
+    }, username)
+
+
+def delete_appointment(dbo, username, appointmentid):
+    """
+    Deletes an appointment
+    """
+    dbo.delete("clinicinvoiceitem", "ClinicAppointmentID=%d" % appointmentid, username)
+    dbo.delete("clinicappointment", appointmentid, username)
+
+def insert_invoice_from_form(dbo, username, post):
+    """
+    Creates an invoice item from posted form data
+    """
+    return dbo.insert("clinicinvoiceitem", {
+        "ClinicAppointmentID":      post.integer("appointmentid"),
+        "Description":              post["description"],
+        "Amount":                   post["amount"]
+    }, username)
+
+def update_invoice_from_form(dbo, username, post):
+    """
+    Creates an invoice item from posted form data
+    """
+    return dbo.update("clinicinvoiceitem", post.integer("itemid"), {
+        "ClinicAppointmentID":      post.integer("appointmentid"),
+        "Description":              post["description"],
+        "Amount":                   post["amount"]
+    }, username)
+
+def delete_invoice(dbo, username, itemid):
+    """
+    Deletes an invoice item
+    """
+    dbo.delete("clinicinvoiceitem", itemid, username)
+
---
+++ test/test_clinic.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python env
+
+import unittest
+import base
+
+import clinic
+import utils
+
+class TestClinic(unittest.TestCase):
+ 
+    anid = 0
+    inid = 0
+
+    def setUp(self):
+        data = {
+            "animal": "1",
+            "person": "1",
+            "date": base.today_display(),
+            "time": "12:00:00",
+            "status": "1",
+        }
+        post = utils.PostedData(data, "en")
+        self.anid = clinic.insert_appointment_from_form(base.get_dbo(), "test", post)
+        data = {
+            "appointmentid": str(self.anid),
+            "description": "foo",
+            "amount": "50"
+        }
+        post = utils.PostedData(data, "en")
+        self.inid = clinic.insert_invoice_from_form(base.get_dbo(), "test", post)
+
+    def tearDown(self):
+        clinic.delete_appointment(base.get_dbo(), "test", self.anid)
+        clinic.delete_invoice(base.get_dbo(), "test", self.inid)
+
+    def test_get_appointments_today(self):
+        assert len(clinic.get_appointments_today(base.get_dbo())) > 0
+
+    def test_get_invoice_items(self):
+        assert len(clinic.get_invoice_items(base.get_dbo(), self.anid)) > 0
+
+    def test_appointment_crud(self):
+        data = {
+            "animal": "2",
+            "person": "2",
+            "date": base.today_display(),
+            "time": "08:00:00",
+            "status": "2",
+        }
+        post = utils.PostedData(data, "en")
+        nid = clinic.insert_appointment_from_form(base.get_dbo(), "test", post)
+        data["appointmentid"] = nid
+        clinic.update_appointment_from_form(base.get_dbo(), "test", post)
+        clinic.delete_appointment(base.get_dbo(), "test", nid)
+
+    def test_invoice_crud(self):
+        data = {
+            "appointmentid": "2",
+            "description": "bar",
+            "amount": "1500"
+        }
+        post = utils.PostedData(data, "en")
+        nid = clinic.insert_invoice_from_form(base.get_dbo(), "test", post)
+        data["itemid"] = nid
+        clinic.update_invoice_from_form(base.get_dbo(), "test", post)
+        clinic.delete_invoice(base.get_dbo(), "test", nid)
+
