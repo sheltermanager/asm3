9e251bb6c0a519b630e27baaed64fe69f2e266c8
Robin Rawson-Tetley <robin@curvy.robsdomain>
2018-05-19 16:03:56 +0100

    disable clinic by default and hide option until complete #9

--- src/i18n.py
+++ src/i18n.py
@@ -7,8 +7,8 @@ import time
 # flake8: noqa - we have a lot of locales and this is convenient
 from locales import *
 
-VERSION = "41u [Sat 19 May 15:27:36 BST 2018]"
-BUILD = "05191527"
+VERSION = "41u [Sat 19 May 15:59:17 BST 2018]"
+BUILD = "05191559"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
