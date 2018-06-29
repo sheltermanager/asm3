ac2734b88b2b0eada9bfdcf7664abc059eecf1a3
Robin Rawson-Tetley <robin@curvy.robsdomain>
2018-06-29 15:10:18 +0100

    revert

--- src/i18n.py
+++ src/i18n.py
@@ -7,8 +7,8 @@ import time
 # flake8: noqa - we have a lot of locales and this is convenient
 from locales import *
 
-VERSION = "41u [Fri 29 Jun 14:57:22 BST 2018]"
-BUILD = "06291457"
+VERSION = "41u [Fri 29 Jun 15:08:08 BST 2018]"
+BUILD = "06291508"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
