f046fc88aec39f1e2ec14f3a06b166d8b9e20a99
Robin Rawson-Tetley <robin@curvy.robsdomain>
2018-06-29 14:40:27 +0100

    Anibase publisher should respond to chipRegisterable=false

--- src/i18n.py
+++ src/i18n.py
@@ -7,8 +7,8 @@ import time
 # flake8: noqa - we have a lot of locales and this is convenient
 from locales import *
 
-VERSION = "41u [Mon 25 Jun 09:42:07 BST 2018]"
-BUILD = "06250942"
+VERSION = "41u [Fri 29 Jun 14:35:55 BST 2018]"
+BUILD = "06291435"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
