8c169c17883e9d63f82012c59081378c33cfaedd
Robin Rawson-Tetley <robin@curvy.robsdomain>
2018-07-04 09:23:04 +0100

    update bulgarian (bg) translation

--- src/i18n.py
+++ src/i18n.py
@@ -7,8 +7,8 @@ import time
 # flake8: noqa - we have a lot of locales and this is convenient
 from locales import *
 
-VERSION = "41u [Tue  3 Jul 15:53:09 BST 2018]"
-BUILD = "07031553"
+VERSION = "41u [Wed  4 Jul 09:18:08 BST 2018]"
+BUILD = "07040918"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
