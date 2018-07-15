7c8cd743c053f83c0fac628993a2b04335e8658e
Robin Rawson-Tetley <robin@curvy.robsdomain>
2018-07-15 17:33:05 +0100

    remove obsolete image path line

--- src/i18n.py
+++ src/i18n.py
@@ -7,8 +7,8 @@ import time
 # flake8: noqa - we have a lot of locales and this is convenient
 from locales import *
 
-VERSION = "41u [Sun 15 Jul 17:14:41 BST 2018]"
-BUILD = "07151714"
+VERSION = "41u [Sun 15 Jul 17:28:42 BST 2018]"
+BUILD = "07151728"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
