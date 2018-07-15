ecaad16661c03b36855dadd9cfc0f34d0e8a6909
Robin Rawson-Tetley <robin@curvy.robsdomain>
2018-07-15 17:20:13 +0100

    upgrade leaflet js

--- src/i18n.py
+++ src/i18n.py
@@ -7,8 +7,8 @@ import time
 # flake8: noqa - we have a lot of locales and this is convenient
 from locales import *
 
-VERSION = "41u [Sun 15 Jul 16:21:09 BST 2018]"
-BUILD = "07151621"
+VERSION = "41u [Sun 15 Jul 17:14:41 BST 2018]"
+BUILD = "07151714"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
