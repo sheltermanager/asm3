cd4c35584411b5ae9805307a8bdb7604f156f7f9
Robin Rawson-Tetley <robin@curvy.robsdomain>
2018-08-15 10:02:06 +0100

    add petrescue breed mapping code

--- src/i18n.py
+++ src/i18n.py
@@ -7,8 +7,8 @@ import time
 # flake8: noqa - we have a lot of locales and this is convenient
 from locales import *
 
-VERSION = "41u [Fri 10 Aug 16:24:07 BST 2018]"
-BUILD = "08101624"
+VERSION = "41u [Wed 15 Aug 09:59:50 BST 2018]"
+BUILD = "08150959"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
