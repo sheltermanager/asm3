39232034497be61838d558da4347656ec1204fca
Robin Rawson-Tetley <robin@curvy.robsdomain>
2019-04-12 15:31:05 +0100

    Found should be able to support 9-digit chip numbers starting with 1

--- src/i18n.py
+++ src/i18n.py
@@ -8,8 +8,8 @@ import utils
 # flake8: noqa - we have a lot of locales and this is convenient
 from locales import *
 
-VERSION = "42u [Wed 10 Apr 12:25:40 BST 2019]"
-BUILD = "04101225"
+VERSION = "42u [Fri 12 Apr 15:28:03 BST 2019]"
+BUILD = "04121528"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
