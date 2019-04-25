c2d9f8b72deb58f8fa1f4fdd5234db63e39dd15a
Robin Rawson-Tetley <robin@quiet.robsdomain>
2019-04-25 13:03:36 +0100

    Show ruled lines between incoming form preview fields #590

--- src/i18n.py
+++ src/i18n.py
@@ -8,8 +8,8 @@ import utils
 # flake8: noqa - we have a lot of locales and this is convenient
 from locales import *
 
-VERSION = "42u [Wed 17 Apr 09:09:12 BST 2019]"
-BUILD = "04170909"
+VERSION = "42u [Thu 25 Apr 13:01:12 BST 2019]"
+BUILD = "04251301"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
