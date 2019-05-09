f6434d1161fbe235ddaf1909478ea24458fbde66
Robin Rawson-Tetley <robin@quiet.robsdomain>
2019-05-09 19:01:30 +0100

    Register old-style 10-digit hex HomeAgain chips with Found

--- src/i18n.py
+++ src/i18n.py
@@ -8,8 +8,8 @@ import utils
 # flake8: noqa - we have a lot of locales and this is convenient
 from locales import *
 
-VERSION = "42u [Thu  9 May 14:11:27 BST 2019]"
-BUILD = "05091411"
+VERSION = "42u [Thu  9 May 18:51:46 BST 2019]"
+BUILD = "05091851"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
