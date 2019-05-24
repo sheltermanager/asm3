bf058eaa35675b72a4f516c701eb1e8218fde66a
Robin Rawson-Tetley <robin@quiet.robsdomain>
2019-05-24 08:39:36 +0100

    better text for movement number search

--- src/i18n.py
+++ src/i18n.py
@@ -8,8 +8,8 @@ import utils
 # flake8: noqa - we have a lot of locales and this is convenient
 from locales import *
 
-VERSION = "42u [Fri 24 May 08:25:41 BST 2019]"
-BUILD = "05240825"
+VERSION = "42u [Fri 24 May 08:35:53 BST 2019]"
+BUILD = "05240835"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
