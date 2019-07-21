73b4089b8afb9f22da64d50aa28da591b912f604
Robin Rawson-Tetley <robin@quiet.robsdomain>
2019-07-21 16:05:32 +0100

    Show trial adoption as an adoption status in shelter view

--- src/i18n.py
+++ src/i18n.py
@@ -8,8 +8,8 @@ import utils
 # flake8: noqa - we have a lot of locales and this is convenient
 from locales import *
 
-VERSION = "43u [Sat 20 Jul 18:45:41 BST 2019]"
-BUILD = "07201845"
+VERSION = "43u [Sun 21 Jul 16:03:26 BST 2019]"
+BUILD = "07211603"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
