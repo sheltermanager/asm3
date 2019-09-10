913568ff59e9b576c57c2ad25cd0d21d5896537d
Robin Rawson-Tetley <robin@quiet.robsdomain>
2019-09-10 21:41:46 +0100

    Change order of CSV fields to match PF docs #644

--- src/asm3/i18n.py
+++ src/asm3/i18n.py
@@ -6,8 +6,8 @@ import time
 # flake8: noqa - we have a lot of locales and this is convenient
 from asm3.locales import *
 
-VERSION = "43u [Tue 10 Sep 17:54:58 BST 2019]"
-BUILD = "09101754"
+VERSION = "43u [Tue 10 Sep 21:40:30 BST 2019]"
+BUILD = "09102140"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
