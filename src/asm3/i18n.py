9f8d00f9533f5242fcd07f3e93e3d9809756aaa2
Robin Rawson-Tetley <robin@quiet.robsdomain>
2019-11-02 08:57:00 +0000

    fix bad tokens in hungarian translation

--- src/asm3/i18n.py
+++ src/asm3/i18n.py
@@ -6,8 +6,8 @@ import time
 # flake8: noqa - we have a lot of locales and this is convenient
 from asm3.locales import *
 
-VERSION = "43u [Fri  1 Nov 13:09:36 GMT 2019]"
-BUILD = "11011309"
+VERSION = "43u [Sat  2 Nov 08:54:47 GMT 2019]"
+BUILD = "11020854"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
