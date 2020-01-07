f9c4adaade47ab40f1b21fe9a529185810867829
Robin Rawson-Tetley <robin@quiet.robsdomain>
2020-01-07 20:26:54 +0000

    fix foundanimals after break over numeric prefix

--- src/asm3/i18n.py
+++ src/asm3/i18n.py
@@ -6,8 +6,8 @@ import time
 # flake8: noqa - we have a lot of locales and this is convenient
 from asm3.locales import *
 
-VERSION = "43u [Tue  7 Jan 14:35:25 GMT 2020]"
-BUILD = "01071435"
+VERSION = "43u [Tue  7 Jan 20:24:38 GMT 2020]"
+BUILD = "01072024"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
