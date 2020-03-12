9f342a4dae240a781bce6b96696d44b6b24b5b4a
Robin Rawson-Tetley <robin@quiet.robsdomain>
2020-03-12 12:58:43 +0000

    put 24 at bottom of list for clarity

--- src/asm3/i18n.py
+++ src/asm3/i18n.py
@@ -6,8 +6,8 @@ import time
 # flake8: noqa - we have a lot of locales and this is convenient
 from asm3.locales import *
 
-VERSION = "43u [Thu 12 Mar 12:48:41 GMT 2020]"
-BUILD = "03121248"
+VERSION = "43u [Thu 12 Mar 12:56:24 GMT 2020]"
+BUILD = "03121256"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 MDY = ( "%m/%d/%Y", "%m/%d/%y" )
