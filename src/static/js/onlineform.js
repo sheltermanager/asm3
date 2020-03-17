b61402ccab5c536ad33239438dbfb71a39137342
Robin Rawson-Tetley <robin@quiet.robsdomain>
2020-03-17 13:22:25 +0000

    Show mandatory when browsing online form fields #626

--- src/static/js/onlineform.js
+++ src/static/js/onlineform.js
@@ -77,6 +77,9 @@ $(function() {
                     { field: "FIELDTYPE", display: _("Type"), formatter: function(row) {
                         return common.get_field(fieldtypes, row.FIELDTYPE, "NAME");
                     }},
+                    { field: "MANDATORY", display: _("Mandatory"), formatter: function(row) {
+                        return row.MANDATORY == 1 ? _("Yes") : _("No")
+                    }},
                     { field: "LABEL", display: _("Label") }
                 ]
             };
