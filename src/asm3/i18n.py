1ec2a7a6f45d98405da69d8b92fd10f2dcc90fea
Robin Rawson-Tetley <robin@quiet.robsdomain>
2020-05-02 14:57:39 +0100

    Allow homechecked by/date fields to be on all the time #812

--- src/asm3/i18n.py
+++ src/asm3/i18n.py
@@ -6,8 +6,8 @@ import time
 # flake8: noqa - we have a lot of locales and this is convenient
 from asm3.locales import *
 
-VERSION = "44u [Sat  2 May 10:40:07 BST 2020]"
-BUILD = "05021040"
+VERSION = "44u [Sat  2 May 14:54:30 BST 2020]"
+BUILD = "05021454"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 HDMY = ( "%d-%m-%Y", "%d-%m-%y" )
