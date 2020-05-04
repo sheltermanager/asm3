bab8199f17f552e7cd4f6f74c7d811f16c306f26
Robin Rawson-Tetley <robin@quiet.robsdomain>
2020-05-04 09:03:24 +0100

    show ID on additional field edit screen

--- src/asm3/i18n.py
+++ src/asm3/i18n.py
@@ -6,8 +6,8 @@ import time
 # flake8: noqa - we have a lot of locales and this is convenient
 from asm3.locales import *
 
-VERSION = "44u [Sun  3 May 09:43:03 BST 2020]"
-BUILD = "05030943"
+VERSION = "44u [Mon  4 May 09:00:10 BST 2020]"
+BUILD = "05040900"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 HDMY = ( "%d-%m-%Y", "%d-%m-%y" )
