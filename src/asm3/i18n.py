51a319d23ea4b854fb3caadfb4961a19a5d442da
Robin Rawson-Tetley <robin@quiet.robsdomain>
2020-05-10 13:14:10 +0100

    add help text for owner field #180

--- src/asm3/i18n.py
+++ src/asm3/i18n.py
@@ -6,8 +6,8 @@ import time
 # flake8: noqa - we have a lot of locales and this is convenient
 from asm3.locales import *
 
-VERSION = "44u [Sun 10 May 09:34:48 BST 2020]"
-BUILD = "05100934"
+VERSION = "44u [Sun 10 May 13:11:07 BST 2020]"
+BUILD = "05101311"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 HDMY = ( "%d-%m-%Y", "%d-%m-%y" )
