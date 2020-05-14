fabb2a76971286aad95dd662b5f3988a951358f2
Robin Rawson-Tetley <robin@quiet.robsdomain>
2020-05-14 18:18:08 +0100

    Fix null error when auto-emailing diary notes

--- src/asm3/i18n.py
+++ src/asm3/i18n.py
@@ -6,8 +6,8 @@ import time
 # flake8: noqa - we have a lot of locales and this is convenient
 from asm3.locales import *
 
-VERSION = "44u [Thu 14 May 17:52:03 BST 2020]"
-BUILD = "05141752"
+VERSION = "44u [Thu 14 May 18:15:05 BST 2020]"
+BUILD = "05141815"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 HDMY = ( "%d-%m-%Y", "%d-%m-%y" )
