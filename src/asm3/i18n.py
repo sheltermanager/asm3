6da228acc54e8dc53edff021b3ad4be672c4bd9e
Robin Rawson-Tetley <robin@quiet.robsdomain>
2020-05-28 13:11:16 +0100

    Fall back to old thumbnails if object-fit unavailable #839

--- src/asm3/i18n.py
+++ src/asm3/i18n.py
@@ -6,8 +6,8 @@ import time
 # flake8: noqa - we have a lot of locales and this is convenient
 from asm3.locales import *
 
-VERSION = "44u [Thu 28 May 12:50:13 BST 2020]"
-BUILD = "05281250"
+VERSION = "44u [Thu 28 May 13:06:58 BST 2020]"
+BUILD = "05281306"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 HDMY = ( "%d-%m-%Y", "%d-%m-%y" )
