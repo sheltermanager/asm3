ddd372295ae8c2666c30a2708c125b4d02f9d24d
Robin Rawson-Tetley <robin@quiet.robsdomain>
2020-07-31 18:03:38 +0100

    Add back deleted users that are dispatched ACO #889

--- src/asm3/i18n.py
+++ src/asm3/i18n.py
@@ -6,8 +6,8 @@ import time
 # flake8: noqa - we have a lot of locales and this is convenient
 from asm3.locales import *
 
-VERSION = "44u [Fri 31 Jul 11:04:45 BST 2020]"
-BUILD = "07311104"
+VERSION = "44u [Fri 31 Jul 18:01:08 BST 2020]"
+BUILD = "07311801"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 HDMY = ( "%d-%m-%Y", "%d-%m-%y" )
