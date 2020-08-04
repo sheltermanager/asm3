459ed95a70198bed38dc7092dd9d298f5f603875
Robin Rawson-Tetley <robin@quiet.robsdomain>
2020-08-04 19:59:39 +0100

    more bad keys

--- src/asm3/i18n.py
+++ src/asm3/i18n.py
@@ -6,8 +6,8 @@ import time
 # flake8: noqa - we have a lot of locales and this is convenient
 from asm3.locales import *
 
-VERSION = "44u [Tue  4 Aug 09:41:29 BST 2020]"
-BUILD = "08040941"
+VERSION = "44u [Tue  4 Aug 19:56:08 BST 2020]"
+BUILD = "08041956"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 HDMY = ( "%d-%m-%Y", "%d-%m-%y" )
