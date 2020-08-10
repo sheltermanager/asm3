7dc4a0f5f3552a978b562721de1ec32c31684c73
Robin Rawson-Tetley <robin@quiet.robsdomain>
2020-08-10 09:04:51 +0100

    heartworm negative dogs report

--- src/asm3/i18n.py
+++ src/asm3/i18n.py
@@ -6,8 +6,8 @@ import time
 # flake8: noqa - we have a lot of locales and this is convenient
 from asm3.locales import *
 
-VERSION = "44u [Sun  9 Aug 15:34:16 BST 2020]"
-BUILD = "08091534"
+VERSION = "44u [Sun  9 Aug 16:19:50 BST 2020]"
+BUILD = "08091619"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 HDMY = ( "%d-%m-%Y", "%d-%m-%y" )
