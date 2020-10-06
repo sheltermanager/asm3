3310603d4071d12895ade0a2dfe094187a1b6211
Robin Rawson-Tetley <robin@quiet.robsdomain>
2020-10-06 17:51:59 +0100

    Allow template paths to be specified in rename

--- src/asm3/i18n.py
+++ src/asm3/i18n.py
@@ -6,8 +6,8 @@ import time
 # flake8: noqa - we have a lot of locales and this is convenient
 from asm3.locales import *
 
-VERSION = "44u [Tue  6 Oct 09:36:36 BST 2020]"
-BUILD = "10060936"
+VERSION = "44u [Tue  6 Oct 17:50:38 BST 2020]"
+BUILD = "10061750"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 HDMY = ( "%d-%m-%Y", "%d-%m-%y" )
