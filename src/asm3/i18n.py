bc14b27ec7d53d6182a749aa61f720ca1b8b51e5
Robin Rawson-Tetley <robin@quiet.robsdomain>
2020-10-30 10:45:18 +0000

    Undeletes for document repository and extra images

--- src/asm3/i18n.py
+++ src/asm3/i18n.py
@@ -6,8 +6,8 @@ import time
 # flake8: noqa - we have a lot of locales and this is convenient
 from asm3.locales import *
 
-VERSION = "44u [Fri 30 Oct 09:38:08 GMT 2020]"
-BUILD = "10300938"
+VERSION = "44u [Fri 30 Oct 10:41:44 GMT 2020]"
+BUILD = "10301041"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 HDMY = ( "%d-%m-%Y", "%d-%m-%y" )
