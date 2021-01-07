18eae30f9afae45531fc9d099ab98895fb10de46
Robin Rawson-Tetley <robin@quiet.robsdomain>
2021-01-07 09:23:31 +0000

    allow searching without validation

--- src/asm3/i18n.py
+++ src/asm3/i18n.py
@@ -6,8 +6,8 @@ import time
 # flake8: noqa - we have a lot of locales and this is convenient
 from asm3.locales import *
 
-VERSION = "44u [Thu  7 Jan 09:10:59 GMT 2021]"
-BUILD = "01070910"
+VERSION = "44u [Thu  7 Jan 09:20:13 GMT 2021]"
+BUILD = "01070920"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 HDMY = ( "%d-%m-%Y", "%d-%m-%y" )
