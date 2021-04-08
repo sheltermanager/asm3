b7c2e232885286298784e09648f8fd5fa90dcc3f
Robin Rawson-Tetley <robin@quiet.robsdomain>
2021-04-08 12:39:55 +0100

    44 release

--- src/asm3/i18n.py
+++ src/asm3/i18n.py
@@ -6,8 +6,8 @@ import time
 # flake8: noqa - we have a lot of locales and this is convenient
 from asm3.locales import *
 
-VERSION = "44u [Wed  7 Apr 08:33:29 BST 2021]"
-BUILD = "04070833"
+VERSION = "44 [Thu  8 Apr 12:24:26 BST 2021]"
+BUILD = "04081224"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 HDMY = ( "%d-%m-%Y", "%d-%m-%y" )
