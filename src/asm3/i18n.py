8138d3afff38573765173ebcb46354a85b9b4053
Robin Rawson-Tetley <robin@quiet.robsdomain>
2021-04-20 10:06:42 +0100

    add michigan dept of agriculture report

--- src/asm3/i18n.py
+++ src/asm3/i18n.py
@@ -6,8 +6,8 @@ import time
 # flake8: noqa - we have a lot of locales and this is convenient
 from asm3.locales import *
 
-VERSION = "45 [Thu 15 Apr 14:23:02 BST 2021]"
-BUILD = "04151423"
+VERSION = "45 [Mon 19 Apr 13:27:45 BST 2021]"
+BUILD = "04191327"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 HDMY = ( "%d-%m-%Y", "%d-%m-%y" )
