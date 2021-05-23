e893f58df5fb7650f560feb28becd6e0ff80753b
Robin Rawson-Tetley <robin@quiet.robsdomain>
2021-05-23 17:15:01 +0100

    Add US territories to state code list

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -2393,14 +2393,18 @@ const html = {
 
     /** Returns a list of all US states as a set of option tags */
     states_us_options: function(selected) {
-        let US_STATES = [ ["Alabama","AL"], ["Alaska","AK"], ["Arizona","AZ"], ["Arkansas","AR"], ["California","CA"], ["Colorado","CO"],
-            ["Connecticut","CT"], ["Delaware","DE"], ["Florida","FL"], ["Georgia","GA"], ["Hawaii","HI"], ["Idaho","ID"], ["Illinois","IL"],
-            ["Indiana","IN"], ["Iowa","IA"], ["Kansas","KS"], ["Kentucky","KY"], ["Louisiana","LA"], ["Maine","ME"], ["Maryland","MD"],
-            ["Massachusetts","MA"], ["Michigan","MI"], ["Minnesota","MN"], ["Mississippi","MS"], ["Missouri","MO"], ["Montana","MT"],
-            ["Nebraska","NE"], ["Nevada","NV"], ["New Hampshire","NH"], ["New Jersey","NJ"], ["New Mexico","NM"], ["New York","NY"],
-            ["North Carolina","NC"], ["North Dakota","ND"], ["Ohio","OH"], ["Oklahoma","OK"], ["Oregon","OR"], ["Pennsylvania","PA"],
-            ["Rhode Island","RI"], ["South Carolina","SC"], ["South Dakota","SD"], ["Tennessee","TN"], ["Texas","TX"], ["Utah","UT"],
-            ["Vermont","VT"], ["Virginia","VA"], ["Washington","WA"], ["West Virginia","WV"], ["Wisconsin","WI"],["Wyoming","WY"]
+        let US_STATES = [ ["Alabama","AL"], ["Alaska","AK"], ["Arizona","AZ"], ["Arkansas","AR"], ["American Samoa","AS"], 
+            ["California","CA"], ["Colorado","CO"], ["Connecticut","CT"], ["District of Columbia","DC"], ["Delaware","DE"], 
+            ["Florida","FL"], ["Federated States of Micronesia","FM"], ["Georgia","GA"], ["Guam","GU"], ["Hawaii","HI"], 
+            ["Idaho","ID"], ["Illinois","IL"], ["Indiana","IN"], ["Iowa","IA"], ["Kansas","KS"], ["Kentucky","KY"], 
+            ["Louisiana","LA"], ["Maine","ME"], ["Marshall Islands","MH"], ["Maryland","MD"], ["Massachusetts","MA"], 
+            ["Michigan","MI"], ["Minnesota","MN"], ["Mississippi","MS"], ["Missouri","MO"], ["Montana","MT"], ["Nebraska","NE"], 
+            ["Nevada","NV"], ["New Hampshire","NH"], ["New Jersey","NJ"], ["New Mexico","NM"], ["New York","NY"],
+            ["North Carolina","NC"], ["North Dakota","ND"], ["Northern Mariana Islands","MP"], ["Ohio","OH"], ["Oklahoma","OK"], 
+            ["Oregon","OR"], ["Palau","PW"], ["Pennsylvania","PA"], ["Puerto Rico","PR"], ["Rhode Island","RI"], 
+            ["South Carolina","SC"], ["South Dakota","SD"], ["Tennessee","TN"], ["Texas","TX"], ["Utah","UT"],
+            ["Vermont","VT"], ["Virgin Islands","VI"], ["Virginia","VA"], ["Washington","WA"], ["West Virginia","WV"], 
+            ["Wisconsin","WI"],["Wyoming","WY"]
         ], opts = [ '<option value=""></option>' ];
         $.each(US_STATES, function(i, v) {
             let sel = common.iif(selected == v[1], 'selected="selected"', '');
