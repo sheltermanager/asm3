690765fb46a18d52e4ad68f6352d32d9469f5214
Robin Rawson-Tetley <robin@quiet.robsdomain>
2021-07-21 10:37:23 +0100

    rearrange dafm as requested

--- src/asm3/i18n.py
+++ src/asm3/i18n.py
@@ -6,8 +6,8 @@ import time
 # flake8: noqa - we have a lot of locales and this is convenient
 from asm3.locales import *
 
-VERSION = "45u [Mon 19 Jul 11:25:48 BST 2021]"
-BUILD = "07191125"
+VERSION = "45u [Tue 20 Jul 09:32:09 BST 2021]"
+BUILD = "07200932"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 HDMY = ( "%d-%m-%Y", "%d-%m-%y" )
