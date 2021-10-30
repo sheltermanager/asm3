78ed2f2138908cba61295a7a4737418bc2be3e49
Robin Rawson-Tetley <robin@quiet.robsdomain>
2021-10-30 13:33:53 +0100

    show the revision in the reports list, document revisions #721

--- src/asm3/i18n.py
+++ src/asm3/i18n.py
@@ -6,8 +6,8 @@ import time
 # flake8: noqa - we have a lot of locales and this is convenient
 from asm3.locales import *
 
-VERSION = "45u [Sat 30 Oct 09:39:49 BST 2021]"
-BUILD = "10300939"
+VERSION = "45u [Sat 30 Oct 13:17:59 BST 2021]"
+BUILD = "10301317"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 HDMY = ( "%d-%m-%Y", "%d-%m-%y" )
