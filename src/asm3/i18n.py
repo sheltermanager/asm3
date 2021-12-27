a70c00381b80a3d29c0cff2cb27a975d8cd4097b
Robin Rawson-Tetley <robin@quiet.robsdomain>
2021-12-27 10:41:48 +0000

    update Hebrew translation

--- src/asm3/i18n.py
+++ src/asm3/i18n.py
@@ -6,8 +6,8 @@ import time
 # flake8: noqa - we have a lot of locales and this is convenient
 from asm3.locales import *
 
-VERSION = "45u [Mon 27 Dec 10:23:20 GMT 2021]"
-BUILD = "12271023"
+VERSION = "45u [Mon 27 Dec 10:41:17 GMT 2021]"
+BUILD = "12271041"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 HDMY = ( "%d-%m-%Y", "%d-%m-%y" )
