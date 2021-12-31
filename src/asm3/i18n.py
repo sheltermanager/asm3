4f8cda953aec0fd5988c6ce00d1d6ef370cfe59c
Robin Rawson-Tetley <robin@quiet.robsdomain>
2021-12-31 11:22:37 +0000

    active litters excluding blank expiries

--- src/asm3/i18n.py
+++ src/asm3/i18n.py
@@ -6,8 +6,8 @@ import time
 # flake8: noqa - we have a lot of locales and this is convenient
 from asm3.locales import *
 
-VERSION = "45u [Fri 31 Dec 10:37:14 GMT 2021]"
-BUILD = "12311037"
+VERSION = "45u [Fri 31 Dec 11:16:30 GMT 2021]"
+BUILD = "12311116"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 HDMY = ( "%d-%m-%Y", "%d-%m-%y" )
