302b543fe25e54cd3c9cfed22b0efd027ce81cc3
Robin Rawson-Tetley <robin@quiet.robsdomain>
2022-02-07 11:54:01 +0000

    still missed a SL field

--- src/asm3/i18n.py
+++ src/asm3/i18n.py
@@ -6,8 +6,8 @@ import time
 # flake8: noqa - we have a lot of locales and this is convenient
 from asm3.locales import *
 
-VERSION = "45u [Mon  7 Feb 10:03:35 GMT 2022]"
-BUILD = "02071003"
+VERSION = "45u [Mon  7 Feb 11:47:00 GMT 2022]"
+BUILD = "02071147"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 HDMY = ( "%d-%m-%Y", "%d-%m-%y" )
