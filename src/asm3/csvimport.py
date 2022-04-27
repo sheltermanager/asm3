f551406d3f6c385eb5b364b18694cc4f018d91f6
Peter Van Caeseele <peter_vc@hotmail.com>
2022-04-27 02:58:39 -0400

    Add CSV import of tattoo information.

--- src/asm3/csvimport.py
+++ src/asm3/csvimport.py
@@ -477,6 +477,9 @@ def csvimport(dbo, csvdata, encoding = "utf-8-sig", user = "", createmissinglook
             a["microchipnumber"] = gks(row, "ANIMALMICROCHIP")
             if a["microchipnumber"] != "": a["microchipped"] = "on"
             a["microchipdate"] = gkd(dbo, row, "ANIMALMICROCHIPDATE")
+            a["tattoonumber"] = gks(row, "ANIMALTATTOO")
+            if a["tattoonumber"] != "": a["tattoo"] = "on"
+            a["tattoodate"] = gkd(dbo, row, "ANIMALTATTOODATE")
             a["flags"] = gks(row, "ANIMALFLAGS")
             a["declawed"] = gkbc(row, "ANIMALDECLAWED")
             a["specialneeds"] = gkbc(row, "ANIMALHASSPECIALNEEDS")
