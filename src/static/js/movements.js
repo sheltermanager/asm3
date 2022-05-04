4f9275ce6afa500a9d29fb326bf298f37c2915dd
Robin Rawson-Tetley <robin@quiet.robsdomain>
2022-05-04 15:32:43 +0100

    No longer hide the person field for TNR/Release movements

--- src/static/js/movements.js
+++ src/static/js/movements.js
@@ -836,7 +836,7 @@ $(function() {
                 $("#returndate").closest("tr").fadeIn();
             }
             // If the movement is one that doesn't require a person, hide the person row
-            if (mt == 4 || mt == 6 || mt == 7) {
+            if (mt == 4 || mt == 6) {
                 $("#person").closest("tr").fadeOut();
             }
             else {
