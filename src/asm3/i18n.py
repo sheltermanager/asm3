64d534f6796b828074fe683ddee617dc1e89e52d
Robin Rawson-Tetley <robin@quiet.robsdomain>
2022-05-10 09:52:21 +0100

    update translation template

--- src/asm3/i18n.py
+++ src/asm3/i18n.py
@@ -6,8 +6,8 @@ import time
 # flake8: noqa - we have a lot of locales and this is convenient
 from asm3.locales import *
 
-VERSION = "46u [Mon  9 May 14:04:28 BST 2022]"
-BUILD = "05091404"
+VERSION = "46u [Tue 10 May 09:51:34 BST 2022]"
+BUILD = "05100951"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 HDMY = ( "%d-%m-%Y", "%d-%m-%y" )
