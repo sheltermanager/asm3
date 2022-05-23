4e33dc6b68c209acce5ae1ea0985657ea6b87457
Robin Rawson-Tetley <robin@office.robsdomain>
2022-05-23 17:05:44 +0100

    last commit for #1096

--- src/static/js/mobile2.js
+++ src/static/js/mobile2.js
@@ -173,6 +173,8 @@ $(document).ready(function() {
         '<div class="list-group">',
         '</div>',
         '</div>',
+        '<div id="content-animal" class="container" style="display: none">',
+        '</div>',
 
         '<div id="content-checklicence" class="container" style="display: none">',
         '<h2>' + _("Check License") + '</h2>',
@@ -190,29 +192,80 @@ $(document).ready(function() {
 
     $("body").html(h);
 
-                /*
-        '<div id="menu-container" class="container">',
-            '<ul class="list-group">',
-                '<li class="list-group-item">Shelter Animals',
-                    '<span class="badge bg-primary rounded-pill">' + controller.animals.length + '</span>',
-                '</li>',
-            '</ul>',
-        '</div>'
-        */
+    // Returns the HTML for rendering an animal record
+    const render_animal = function(a) {
+        
+        const i = function(label, value) {
+            if (!value) { value = ""; }
+            return '<div class="row align-items-start"><div class="col">' + label + '</div><div class="col">' + value + '</div></div>';
+        }
+        let [adoptable, adoptreason] = html.is_animal_adoptable(a);
+        let h = [
+            '<div class="list-group" style="margin-top: 5px">',
+            '<a href="#" data-link="shelteranimals" class="list-group-item list-group-item-action internal-link">',
+            '&#8592; ' + _("Back"),
+            '</a>',
+            '<div class="list-group-item">',
+            '<img style="float: right" src="' + html.thumbnail_src(a, "animalthumb") + '">',
+            '<h5 class="mb-1">' + a.ANIMALNAME + ' - ' + a.CODE + '</h5>',
+            '<small>' + common.substitute(_("{0} {1} {2} aged {3}"), { "0": a.SEXNAME, "1": a.BREEDNAME, "2": a.SPECIESNAME, "3": a.ANIMALAGE }) + '<br/>',
+            a.IDENTICHIPNUMBER + '</small><br/>',
+            '<input type="file" accept="image/*" id="uploadphoto" />',
+            '</div>',
+            '</div>',
 
+            i(_("Status"), adoptable ? _("Available for adoption") : _("Not available for adoption") + " (" + adoptreason + ")"),
+            i(_("Type"), a.ANIMALTYPENAME),
+            i(_("Location"), a.DISPLAYLOCATION),
+            i(_("Color"), a.BASECOLOURNAME),
+            i(_("Coat Type"), a.COATTYPENAME),
+            i(_("Size"), a.SIZENAME),
+            i(_("DOB"), format.date(a.DATEOFBIRTH) + " (" + a.ANIMALAGE + ")"),
+            
+            i(_("Markings"), a.MARKINGS),
+            i(_("Hidden Comments"), a.HIDDENANIMALDETAILS),
+            i(_("Description"), a.ANIMALCOMMENTS),
+            
+            i(_("Cats"), a.ISGOODWITHCATSNAME),
+            i(_("Dogs"), a.ISGOODWITHDOGSNAME),
+            i(_("Children"), a.ISGOODWITHCHILDRENNAME),
+            i(_("Housetrained"), a.ISHOUSETRAINEDNAME),
+            
+            common.has_permission("vo") ? i(_("Original Owner"), a.ORIGINALOWNERNAME) : "",
+            common.has_permission("vo") ? i(_("Brought In By"), a.BROUGHTINBYOWNERNAME) : "",
+
+            i(_("Date Brought In"), format.date(a.DATEBROUGHTIN)),
+            i(_("Bonded With"), a.BONDEDANIMAL1CODE + " " + a.BONDEDANIMAL1NAME + " " + a.BONDEDANIMAL2CODE + " " + a.BONDEDANIMAL2NAME),
+            i(_("Transfer"), a.ISTRANSFERNAME),
+            i(_("Entry Category"), a.ENTRYREASONNAME),
+            i(_("Entry Reason"), a.REASONFORENTRY),
+
+            i(_("Microchipped"), format.date(a.IDENTICHIPDATE) + " " + a.IDENTICHIPPED==1 ? a.IDENTICHIPNUMBER : ""),
+            i(_("Tattoo"), format.date(a.TATTOODATE) + " " + a.TATTOO==1 ? a.TATTOONUMBER : ""),
+            i(_("Neutered"), a.NEUTEREDNAME + " " + format.date(a.NEUTEREDDATE)),
+            i(_("Declawed"), a.DECLAWEDNAME),
+            i(_("Heartworm Tested"), format.date(a.HEARTWORMTESTDATE) + " " + a.HEARTWORMTESTED==1 ? a.HEARTWORMTESTRESULTNAME : ""),
+            i(_("FIV/L Tested"), format.date(a.COMBITESTDATE) + " " + a.COMBITESTED==1 ? a.COMBITESTRESULTNAME + " " + a.FLVRESULTNAME : ""),
+            i(_("Health Problems"), a.HEALTHPROBLEMS),
+            i(_("Rabies Tag"), a.RABIESTAG),
+            i(_("Special Needs"), a.HASSPECIALNEEDSNAME),
+            i(_("Current Vet"), a.CURRENTVETNAME + " " + a.CURRENTVETWORKTELEPHONE)
+        ];
+        return h.join("\n");
+    };
 
     // Hide all the elements with hideifzero if they have a badge containing zero
     $(".hideifzero").each(function() {
         $(this).toggle( $(this).find("span.badge").text() != "0" );
     });
 
-    // Event handlers for internal links
-    $(".internal-link").each(function() {
+    // Delegate handler for internal links
+    $("body").on("click", ".internal-link", function() {
         let target = $(this).attr("data-link");
-        $(this).click(function() {
+        if (target) {
             $(".container").hide();
             $("#content-" + target).show();
-        });
+        }
     });
 
     // Make the mobile submenu collapse when an internal link is clicked
@@ -223,12 +276,28 @@ $(document).ready(function() {
     // Load shelter animals list
     $("#content-shelteranimals .list-group").empty();
     $.each(controller.animals, function(i, v) {
-        let h = '<a href="#" class="list-group-item list-group-item-action">' +
+        let h = '<a href="#" data-id="' + v.ID + '" class="list-group-item list-group-item-action">' +
             '<img style="float: right" height="75px" src="' + html.thumbnail_src(v, "animalthumb") + '">' + 
             '<h5 class="mb-1">' + v.ANIMALNAME + ' - ' + v.CODE + '</h5>' +
             '<small>(' + v.SEXNAME + ' ' + v.BREEDNAME + ' ' + v.SPECIESNAME + ') ' + v.IDENTICHIPNUMBER + '</small>' +
             '</a>';
         $("#content-shelteranimals .list-group").append(h);
+    });
+    // When a shelter animal link is clicked, display the record
+    $("#content-shelteranimals").on("click", "a", function() {
+        let animalid = format.to_int($(this).attr("data-id")), a = null;
+        $.each(controller.animals, function(i, v) {
+            if (v.ID == animalid) { a = v; return false; }
+        });
+        if (a) { 
+            $("#content-animal").html( render_animal(a) ); 
+            $(".container").hide();
+            $("#content-animal").show();
+            // Handle the uploading of a photo when one is chosen
+            $("#uploadphoto").change(function() {
+                alert($("#uploadphoto").val());
+            });
+        }
     });
 
     // Load messages 
