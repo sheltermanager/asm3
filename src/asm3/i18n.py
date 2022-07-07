18582257836b7da187256575a6b034d948c6b81f
Robin Rawson-Tetley <robin@office.robsdomain>
2022-07-07 09:44:05 +0100

    add reserves by animal

--- src/asm3/i18n.py
+++ src/asm3/i18n.py
@@ -6,8 +6,8 @@ import time
 # flake8: noqa - we have a lot of locales and this is convenient
 from asm3.locales import *
 
-VERSION = "46u [Thu  7 Jul 08:23:04 BST 2022]"
-BUILD = "07070823"
+VERSION = "46u [Thu  7 Jul 09:19:18 BST 2022]"
+BUILD = "07070919"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 HDMY = ( "%d-%m-%Y", "%d-%m-%y" )
