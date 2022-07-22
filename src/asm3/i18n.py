03de38a34e91f2821f2dab39a00865310c8e2b4b
Robin Rawson-Tetley <robin@office.robsdomain>
2022-07-22 10:58:26 +0100

    Retry and error notification mechanism for failed S3 PUT #1155

--- src/asm3/i18n.py
+++ src/asm3/i18n.py
@@ -6,8 +6,8 @@ import time
 # flake8: noqa - we have a lot of locales and this is convenient
 from asm3.locales import *
 
-VERSION = "46u [Fri 22 Jul 08:13:42 BST 2022]"
-BUILD = "07220813"
+VERSION = "46u [Fri 22 Jul 10:54:41 BST 2022]"
+BUILD = "07221054"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 HDMY = ( "%d-%m-%Y", "%d-%m-%y" )
