89c3bdad871bccb0deed01157149b1392f6cdfbc
Robin Rawson-Tetley <robin@rawsontetley.org>
2023-04-13 12:07:29 +0100

    ... and stockusagetype

--- src/asm3/dbupdate.py
+++ src/asm3/dbupdate.py
@@ -108,9 +108,9 @@ TABLES_LOOKUP = ( "accounts", "additionalfield", "animaltype", "basecolour", "br
     "lksdiarylink", "lksdonationfreq", "lksex", "lksfieldlink", "lksfieldtype", "lksize", "lksloglink", 
     "lksmedialink", "lksmediatype", "lksmovementtype", "lksoutcome", "lksposneg", "lksrotatype", "lksyesno", 
     "lksynun", "lksynunk", "lkstransportstatus", "lkurgency", "lkworktype", "logtype", "medicalprofile", 
-    "onlineform", "onlineformfield", "pickuplocation", "reservationstatus", "site", "stocklocation", "species", 
-    "templatedocument", "templatehtml", "testtype", "testresult", "transporttype", "traptype", 
-    "vaccinationtype", "voucher" )
+    "onlineform", "onlineformfield", "pickuplocation", "reservationstatus", "site", "stocklocation", 
+    "stockusagetype", "species", "templatedocument", "templatehtml", "testtype", "testresult", 
+    "transporttype", "traptype", "vaccinationtype", "voucher" )
 
 VIEWS = ( "v_adoption", "v_animal", "v_animalcontrol", "v_animalfound", "v_animallost", 
     "v_animalmedicalcombined", "v_animalmedicaltreatment", "v_animaltest", "v_animalvaccination", 
