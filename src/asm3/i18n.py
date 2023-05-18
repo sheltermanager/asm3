81eefb954c4e8a032d8e4125fcca951fc355dfcb
Robin Rawson-Tetley <robin@rawsontetley.org>
2023-05-18 09:37:16 +0100

    Update tablesorter to 2.31.3

--- src/asm3/i18n.py
+++ src/asm3/i18n.py
@@ -7,8 +7,8 @@ import time
 from asm3.locales import *
 
 
-VERSION = "47u [Wed 17 May 15:12:29 BST 2023]"
-BUILD = "05171512"
+VERSION = "47u [Thu 18 May 09:35:18 BST 2023]"
+BUILD = "05180935"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 HDMY = ( "%d-%m-%Y", "%d-%m-%y" )
