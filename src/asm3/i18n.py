ba79b9803c1e5d26cf435490aae979750307e65c
Robin Rawson-Tetley <robin@rawsontetley.org>
2023-06-07 22:25:36 +0100

    Use a lighter background for custom emblems

--- src/asm3/i18n.py
+++ src/asm3/i18n.py
@@ -7,8 +7,8 @@ import time
 from asm3.locales import *
 
 
-VERSION = "47u [Wed  7 Jun 11:47:11 BST 2023]"
-BUILD = "06071147"
+VERSION = "47u [Wed  7 Jun 22:16:52 BST 2023]"
+BUILD = "06072216"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 HDMY = ( "%d-%m-%Y", "%d-%m-%y" )
