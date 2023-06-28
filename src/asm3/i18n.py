dee434f7ac0182974e5f0c598382674b2803b9da
Robin Rawson-Tetley <robin@rawsontetley.org>
2023-06-28 09:50:00 +0100

    mode security fix broke converting jpg2pdf

--- src/asm3/i18n.py
+++ src/asm3/i18n.py
@@ -7,8 +7,8 @@ import time
 from asm3.locales import *
 
 
-VERSION = "47u [Tue 27 Jun 17:43:31 BST 2023]"
-BUILD = "06271743"
+VERSION = "47u [Wed 28 Jun 09:49:03 BST 2023]"
+BUILD = "06280949"
 
 DMY = ( "%d/%m/%Y", "%d/%m/%y" )
 HDMY = ( "%d-%m-%Y", "%d-%m-%y" )
