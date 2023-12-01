83184166f76356b0da193234b588bece070b4b5f
Robin Rawson-Tetley <robin@rawsontetley.org>
2023-12-01 17:14:40 +0000

    Fix error generating report HTML with no columns

--- src/asm3/reports.py
+++ src/asm3/reports.py
@@ -403,7 +403,9 @@ def generate_html(dbo: Database, username: str, sql: str) -> str:
     rs, cols = dbo.query_tuple_columns(sql)
     h = "$$HEADER\n<table border=\"1\">\n<tr>\n"
     b = "$$BODY\n<tr>\n"
-    f = "$$FOOTER\n</table>\n<p>Total: {COUNT.%s}</p>\nFOOTER$$\n" % cols[0].upper()
+    f = "$$FOOTER\n</table>\nFOOTER$$\n"
+    if len(cols) > 0:
+        f = "$$FOOTER\n</table>\n<p>Total: {COUNT.%s}</p>\nFOOTER$$\n" % cols[0].upper()
     for c in cols:
         h += "<th>%s</th>\n" % c
         b += "<td>$%s</td>\n" % c.upper()
