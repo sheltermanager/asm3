feb2deffeff65a1b0ccaa9c5e7c199492ba8a114
Robin Rawson-Tetley <robin@rawsontetley.org>
2024-03-05 10:40:41 +0000

    Translate publisher names on publishing logs screen

--- src/static/js/publish_logs.js
+++ src/static/js/publish_logs.js
@@ -6,6 +6,27 @@ $(function() {
 
     const publish_logs = {
 
+        PUBLISHER_NAMES:  {
+            "adoptapet":        "adoptapet.com",
+            "akcreunite":       "AKC Reunite",
+            "anibaseuk":        "Anibase UK",
+            "buddyid":          "Buddy ID",
+            "foundanimals":     "Found/24Pet",
+            "helpinglostpets":  "helpinglostpets.com",
+            "homeagain":        "HomeAgain",
+            "maddiesfund":      "Maddies Pet Assistant",
+            "petcademy":        "Petcademy",
+            "petfinder":        "petfinder.com",
+            "petlink":          "petlink.net",
+            "petrescue":        "petrescue.com.au",
+            "petslocateduk":    "petslocated.co.uk",
+            "pettracuk":        "AVID PETtrac",
+            "rescuegroups":     "rescuegroups.org",
+            "sacmetrics":       "shelteranimalscount.org",
+            "savourlife":       "savour-life.com.au",
+            "smarttag":         "Smart Tag"
+        },
+
         model: function() {
             const table = {
                 rows: controller.rows,
@@ -14,7 +35,9 @@ $(function() {
                     common.route("publish_log_view?ajax=false&view=" + row.ID);
                 },
                 columns: [
-                    { field: "NAME", display: _("Publisher") },
+                    { field: "NAME", display: _("Publisher"), formatter: function(row) {
+                        return PUBLISHER_NAMES[row.NAME] || row.NAME;
+                    }},
                     { field: "PUBLISHDATETIME", display: _("Date"), formatter: tableform.format_datetime, initialsort: true, initialsortdirection: "desc" },
                     { field: "SUCCESS", display: _("Success") },
                     { field: "ALERTS", display: _("Alerts") }
