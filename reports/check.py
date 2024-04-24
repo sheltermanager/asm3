aa172eb303bc294a673f3fff735625bce63683b0
Robin Rawson-Tetley <robin@rawsontetley.org>
2024-04-24 13:40:31 +0100

    make check.py construct and validate the HTML portion as XML

--- reports/check.py
+++ reports/check.py
@@ -57,6 +57,52 @@ def substitute(sql):
         i = sql.find("$", i+1)
     return sql.strip()
 
+_html_parser = None
+def validate_html(html):
+    """If lxml can properly parse the html, return the lxml representation.
+    Otherwise raise."""
+    global _html_parser
+    from lxml import etree
+    from io import StringIO
+    if not _html_parser: _html_parser = etree.HTMLParser(recover = False)
+    return etree.parse(StringIO(html), _html_parser)
+
+def check_html(h):
+    def get_section(startpattern, endpattern, nth=1):
+        ss = h.find(startpattern)
+        es = h.find(endpattern, ss)
+        if nth == 2:
+            ss = h.find(startpattern, es)
+            es = h.find(endpattern, ss)
+        if ss == -1: 
+            return ""
+        if es == -1:
+            print(f"missing end tag {endpattern}")
+            return ""
+        return h[ss + len(startpattern):es]
+    def get_groupheadfoot(g):
+        gh = g.find("$$HEAD")
+        gf = g.find("$$FOOT")
+        if gh == -1 or gf == -1:
+            print(f"can't find $$HEAD or $$FOOT in group '{g}'")
+        return g[gh+6:gf], g[gf+6:]
+    header = get_section("$$HEADER", "HEADER$$")
+    footer = get_section("$$FOOTER", "FOOTER$$")
+    body = get_section("$$BODY", "BODY$$")
+    group1 = get_section("$$GROUP", "GROUP$$", 1)
+    group2 = get_section("$$GROUP", "GROUP$$", 2)
+    group1head = ""
+    group1foot = ""
+    group2head = ""
+    group2foot = ""
+    if group1 != "": group1head, group1foot = get_groupheadfoot(group1)
+    if group2 != "": group2head, group2foot = get_groupheadfoot(group2)
+    doc = f"{header}\n{group1head}\n{group2head}\n{body}\n{group2foot}\n{group1foot}\n{footer}"
+    try:
+        validate_html(doc)
+    except Exception as e:
+        print("\nHTML VALIDATION FAILED: %s" % e)
+
 def parse_reports(data):
     global checked
     global total
@@ -83,6 +129,8 @@ def parse_reports(data):
             except Exception as err:
                 print(f"\nQUERY FAILED: {err}\n")
                 print(sql)
+            if len(html) > 0 and html.find("<") != -1:
+                check_html(html)
             if subreports:
                 elem=0
                 name = ""
@@ -104,6 +152,9 @@ def parse_reports(data):
                         except Exception as err:
                             print(f"\nQUERY FAILED: {err}\n")
                             print(sql)
+                        if len(html) > 0 and html.find("<") != -1:
+                            check_html(html)
+
         elif EXECPOSTGRES and dbinfo.find("PostgreSQL") != -1:
             with open("zzz_check.sql", "w") as f:
                 f.write(substitute(sql))
@@ -111,6 +162,8 @@ def parse_reports(data):
             os.system("scp -q zzz_check.sql root@eur05ddx.sheltermanager.com:/root/")
             os.system("ssh root@eur05ddx.sheltermanager.com \"psql -q -U robin -f zzz_check.sql > /dev/null && rm -f zzz_check.sql\"")
             os.system("rm -f zzz_check.sql")
+            if len(html) > 0 and html.find("<") != -1:
+                check_html(html)
         else:
             if SHOWNONEXEC:
                 print(f"Cannot execute query for {dbinfo}, copy and paste query below:\n")
@@ -125,4 +178,4 @@ for f in sys.argv:
         with open(f, "r") as h:
             parse_reports(h.read())
 
-print(f"\nChecked {checked} / {total} queries.")
+print(f"\nChecked {checked} / {total} reports.")
