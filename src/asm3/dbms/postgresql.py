0c43914a3c0bde94524116af96730a46262f9586
Robin Rawson-Tetley <robin@rawsontetley.org>
2024-12-17 17:42:34 +0000

    Add explicit postgresql type casts to SQL TO_CHAR function #1517

--- src/asm3/dbms/postgresql.py
+++ src/asm3/dbms/postgresql.py
@@ -122,7 +122,7 @@ class DatabasePostgreSQL(Database):
     
     def sql_datetochar(self, fieldexpr: str, formatstr: str) -> str:
         """ Writes an expression that formats a date, valid format tokens YYYY MM DD HH NN SS """
-        return f"TO_CHAR({fieldexpr}, '{formatstr}')"
+        return f"TO_CHAR({fieldexpr}::timestamp, '{formatstr}')"
     
     def sql_datexday(self, dateexpr: str) -> str:
         """
