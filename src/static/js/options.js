a2a5e9ce22a49847b9fa83fd0721c6fdd69b7123
Robin Rawson-Tetley <robin@sheltermanager.com>
2025-01-20 11:05:28 +0000

    change spambot name mixed case to reflect what it does

--- src/static/js/options.js
+++ src/static/js/options.js
@@ -1471,7 +1471,7 @@ $(function() {
                 '<input data="AutoHashProcessedForms" id="hashprocessedforms" class="asm-checkbox" type="checkbox" /> <label for="hashprocessedforms">' + _("When storing processed forms as media, apply tamper proofing and make them read only") + '</label><br/>',
                 '<input data="OnlineFormSpamHoneyTrap" id="spamhoneytrap" class="asm-checkbox" type="checkbox" /> <label for="spamhoneytrap">' + _("Spambot protection: Invisible checkbox") + '</label><br/>',
                 '<input data="OnlineFormSpamUACheck" id="spamuacheck" class="asm-checkbox" type="checkbox" /> <label for="spamuacheck">' + _("Spambot protection: UserAgent check") + '</label><br/>',
-                '<input data="OnlineFormSpamFirstnameMixCase" id="spamfirstname" class="asm-checkbox" type="checkbox" /> <label for="spamfirstname">' + _("Spambot protection: First name mixed case") + '</label><br/>',
+                '<input data="OnlineFormSpamFirstnameMixCase" id="spamfirstname" class="asm-checkbox" type="checkbox" /> <label for="spamfirstname">' + _("Spambot protection: Person name mixed case") + '</label><br/>',
                 '<input data="OnlineFormSpamPostcode" id="spampostcode" class="asm-checkbox" type="checkbox" /> <label for="spampostcode">' + _("Spambot protection: Zipcode contains numbers") + '</label><br/>',
                 '</p>',
                 '</div>'
