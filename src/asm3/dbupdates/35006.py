bddb28c1aa95523b023e774ffaf8487bc918517b
Adam Spencer <adamdspencer77@gmail.com>
2025-04-16 15:50:36 +0100

    Done. #1728

--- src/asm3/dbupdates/35006.py
+++ src/asm3/dbupdates/35006.py
@@ -1,7 +1,10 @@
-from asm3.dbupdate import add_column, add_index
+from asm3.dbupdate import add_column, add_index, execute
 
 add_column(dbo, "animal", "IdentichipStatus", dbo.type_integer)
 add_column(dbo, "animal", "Identichip2Status", dbo.type_integer)
 
 add_index(dbo, "animal_IdentichipStatus", "animal", "IdentichipStatus")
 add_index(dbo, "animal_Identichip2Status", "animal", "Identichip2Status")
+
+execute(dbo, "UPDATE animal SET IdentichipStatus=0, Identichip2Status=0")
+execute(dbo, "INSERT INTO configuration (ItemName, ItemValue) VALUES (?, ?)", ["DontShowMicrochipStatus", "Yes"])
