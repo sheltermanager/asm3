db293a71192f24d6251f70bed720c4c7dec218a7
Robin Rawson-Tetley <robin@sheltermanager.com>
2025-05-12 10:47:48 +0100

    move update 35005 to 35008 #1727

--- src/asm3/dbupdates/35005.py
+++
@@ -1,4 +0,0 @@
-from asm3.dbupdate import execute, add_column
-# Add the new phone number additional field type
-add_column(dbo, "onlineformfield", "ValidationRule", dbo.type_integer)
-
---
+++ src/asm3/dbupdates/35008.py
@@ -0,0 +1,4 @@
+from asm3.dbupdate import add_column
+
+add_column(dbo, "onlineformfield", "ValidationRule", dbo.type_integer)
+
