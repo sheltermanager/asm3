92ab599eac67b72ad727232f935bb20fc5e6fc5f
Adam Spencer <adamdspencer77@gmail.com>
2025-06-30 09:25:31 +0100

    Tidied code following feedback. #444

--- src/asm3/dbupdates/35011.py
+++ src/asm3/dbupdates/35011.py
@@ -1,4 +1,10 @@
 from asm3.dbupdate import execute
 
-execute(dbo, "CREATE TABLE lkclinicinvoiceitems (ID %(int)s NOT NULL PRIMARY KEY, ClinicInvoiceItemName %(short)s, ClinicInvoiceItemDescription %(long)s, DefaultCost %(int)s, IsRetired %(int)s)" % { "int": dbo.type_integer, "short": dbo.type_shorttext, "long": dbo.type_longtext })
-
+fields = ",".join([
+    dbo.ddl_add_table_column("ID", dbo.type_integer, False, pk=True),
+    dbo.ddl_add_table_column("ClinicInvoiceItemName", dbo.type_shorttext, False),
+    dbo.ddl_add_table_column("ClinicInvoiceItemDescription", dbo.type_longtext, True),
+    dbo.ddl_add_table_column("DefaultCost", dbo.type_integer, False),
+    dbo.ddl_add_table_column("IsRetired", dbo.type_integer, False)
+])
+execute(dbo, dbo.ddl_add_table("lkclinicinvoiceitems", fields) )
