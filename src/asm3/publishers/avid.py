385036fe4b3133ad56b2547b3caf53febdcbc0ee
Adam Spencer <adamdspencer77@gmail.com>
2025-09-05 10:33:36 +0100

    More tweaking. #1785

--- src/asm3/publishers/avid.py
+++ src/asm3/publishers/avid.py
@@ -105,11 +105,11 @@ class AVIDUSPublisher(AbstractPublisher):
                     return
                 if not self.validate(an): continue
                 fields = self.processAnimal(an, registeroverseas)
-                self.log("HTTP POST request %s: %s" % (AVID_US_POST_URL, str(fields)))
+                #self.log("HTTP POST request %s: %s" % (AVID_US_POST_URL, str(fields)))
                 r = asm3.utils.post_form(AVID_US_POST_URL, fields, config['headers'])
                 #r = requests.post(AVID_US_POST_URL, json=fields, headers=config['headers'])
                 #self.log("HTTP response: %s" % str(r.json()))#r["response"])
-                self.log("HTTP response: %s" % r["response"])
+                self.log("HTTP response: %s" % str(r))
                 #self.log("HTTP status: %s" % r["status"])
                 #self.log("HTTP headers: %s" % r["headers"])
 
