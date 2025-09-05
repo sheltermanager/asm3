1d0ca2773e743bf0b10ce7273c5ffee571204108
Adam Spencer <adamdspencer77@gmail.com>
2025-09-05 10:55:06 +0100

    More tweaking. #1785

--- src/asm3/publishers/avid.py
+++ src/asm3/publishers/avid.py
@@ -107,7 +107,7 @@ class AVIDUSPublisher(AbstractPublisher):
                 fields = self.processAnimal(an, registeroverseas)
                 #self.log("HTTP POST request %s: %s" % (AVID_US_POST_URL, str(fields)))
                 #r = asm3.utils.post_json(AVID_US_POST_URL, fields, config['headers'])
-                r = requests.post(AVID_US_POST_URL, json=fields, headers=config['headers'])
+                r = requests.post(AVID_US_POST_URL, json=fields, headers=config['headers']).json()
                 #self.log("HTTP response: %s" % str(r.json()))#r["response"])
                 self.log("HTTP response: %s" % str(r))
                 #self.log("HTTP status: %s" % r["status"])
