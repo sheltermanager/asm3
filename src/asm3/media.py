763d7ad1da98b4e3898e25b556e30c92b41da474
adamdspencer77 <58069848+adamdspencer77@users.noreply.github.com>
2025-09-17 10:01:51 +0100

    Signing date/IP/UA placeholders #1327
    * Working prototype. #1327
    
    * Hopefully sorted. #1327

--- src/asm3/media.py
+++ src/asm3/media.py
@@ -774,6 +774,9 @@ def sign_document(dbo: Database, username: str, mid: int, sigurl: str, signdate:
     """
     asm3.al.debug("signing document %s for %s" % (mid, username), "media.sign_document", dbo)
     SIG_PLACEHOLDER = "signature:placeholder"
+    SIG_DATE_PLACEHOLDER = "{SIGNING_DATE}"
+    SIG_IP_PLACEHOLDER = "{SIGNING_IP}"
+    SIG_UA_PLACEHOLDER = "{SIGNING_UA}"
     m = dbo.first_row(dbo.query("SELECT * FROM media WHERE ID=?", [mid]))
     if m is None:  
         raise asm3.utils.ASMValidationError("cannot find media with ID %s" % mid)
@@ -807,6 +810,15 @@ def sign_document(dbo: Database, username: str, mid: int, sigurl: str, signdate:
     if content.find(SIG_PLACEHOLDER) != -1:
         asm3.al.debug("document %s: found signature placeholder" % mid, "media.sign_document", dbo)
         content = content.replace(SIG_PLACEHOLDER, sigurl)
+        if content.find(SIG_DATE_PLACEHOLDER) != -1:
+            asm3.al.debug("document %s: found signature date placeholder" % mid, "media.sign_document", dbo)
+            content = content.replace(SIG_DATE_PLACEHOLDER, signdate)
+        if content.find(SIG_IP_PLACEHOLDER) != -1:
+            asm3.al.debug("document %s: found signature IP placeholder" % mid, "media.sign_document", dbo)
+            content = content.replace(SIG_IP_PLACEHOLDER, remoteip)
+        if content.find(SIG_UA_PLACEHOLDER) != -1:
+            asm3.al.debug("document %s: found signature UA placeholder" % mid, "media.sign_document", dbo)
+            content = content.replace(SIG_UA_PLACEHOLDER, useragent)
     else:
         # Create the signature at the foot of the document
         asm3.al.debug("document %s: no placeholder, appending" % mid, "media.sign_document", dbo)
