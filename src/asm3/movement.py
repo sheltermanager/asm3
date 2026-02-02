d20c89625efd2b740ce33c8fd75bf610cca10eb6
Robin Rawson-Tetley <robin@sheltermanager.com>
2026-02-02 10:14:23 +0000

    Fix error creating costs from move_adopt/reclaim

--- src/asm3/movement.py
+++ src/asm3/movement.py
@@ -705,7 +705,7 @@ def insert_adoption_from_form(dbo: Database, username: str, post: PostedData, cr
     cost_create = post.boolean("costcreate")
     if cost_amount > 0 and cost_type != "" and cost_create:
         boc_dict = {
-            "animalid"          : post["animal"],
+            "animal"            : post["animal"],
             "type"              : cost_type,
             "costdate"          : post["movementdate"],
             "costpaid"          : post["movementdate"],
@@ -803,7 +803,7 @@ def insert_reclaim_from_form(dbo: Database, username: str, post: PostedData) -> 
     cost_create = post.boolean("costcreate")
     if cost_amount > 0 and cost_type != "" and cost_create:
         boc_dict = {
-            "animalid"          : post["animal"],
+            "animal"            : post["animal"],
             "type"              : cost_type,
             "costdate"          : post["movementdate"],
             "costpaid"          : post["movementdate"],
