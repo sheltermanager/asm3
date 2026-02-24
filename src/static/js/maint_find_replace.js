6e0119830c579839e07f240b43483a060495727a
Adam Spencer <adamdspencer77@gmail.com>
2026-02-24 10:15:51 +0000

    New values added to find selector. #2004

--- src/static/js/maint_find_replace.js
+++ src/static/js/maint_find_replace.js
@@ -70,6 +70,17 @@ $(function() {
             $("#replacemanufacturers").click(async function() {
                 let formdata = "mode=replacemanufacturers&" + $("#manufacturerfind, #manufacturerreplace").toPOST();
                 let result = await common.ajax_post("maint_find_replace", formdata);
+                $("#manufacturerfind option:selected").remove();
+                let termpresent = false;
+                $.each($("#manufacturerfind option"), function(i, v) {
+                    if ( $(v).val() == $("#manufacturerreplace").val() ) {
+                        termpresent = true;
+                        return;
+                    }
+                });
+                if (!termpresent) {
+                    $("#manufacturerfind").append('<option>' + $("#manufacturerreplace").val() + '</option>');
+                }
                 header.show_info(_("{0} vaccination record(s) affected").replace("{0}", result));
             });
 
@@ -77,6 +88,17 @@ $(function() {
             $("#replacecities").click(async function() {
                 let formdata = "mode=replacecities&" + $("#cityfind, #cityreplace").toPOST();
                 let result = await common.ajax_post("maint_find_replace", formdata);
+                $("#cityfind option:selected").remove();
+                let termpresent = false;
+                $.each($("#cityfind option"), function(i, v) {
+                    if ( $(v).val() == $("#cityreplace").val() ) {
+                        termpresent = true;
+                        return;
+                    }
+                });
+                if (!termpresent) {
+                    $("#cityfind").append('<option>' + $("#cityreplace").val() + '</option>');
+                }
                 header.show_info(_("{0} person record(s) affected").replace("{0}", result));
             });
 
@@ -84,6 +106,17 @@ $(function() {
             $("#replacestates").click(async function() {
                 let formdata = "mode=replacestates&" + $("#statefind, #statereplace").toPOST();
                 let result = await common.ajax_post("maint_find_replace", formdata);
+                $("#statefind option:selected").remove();
+                let termpresent = false;
+                $.each($("#statefind option"), function(i, v) {
+                    if ( $(v).val() == $("#statereplace").val() ) {
+                        termpresent = true;
+                        return;
+                    }
+                });
+                if (!termpresent) {
+                    $("#statefind").append('<option>' + $("#statereplace").val() + '</option>');
+                }
                 header.show_info(_("{0} person record(s) affected").replace("{0}", result));
             });
 
