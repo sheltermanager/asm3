69602f0c1afecd3e1f5dc03fa5e33f600b8dca0a
Adam Spencer <adamdspencer77@gmail.com>
2026-02-25 15:58:03 +0000

    Fixed menus lingering after menu item chosen on small screens. #1398

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -540,6 +540,7 @@ const common = {
             if (common.route_is_client(href)) {
                 e.preventDefault();
                 common.route(href);
+                if (header.compact_menu) { $("#asm-topline .asm-menu-icon").hide(); }
                 return false;
             }
         });
