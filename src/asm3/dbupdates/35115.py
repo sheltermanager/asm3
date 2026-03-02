1508a0035641167113bb715de2c235dafcb59266
Robin Rawson-Tetley <robin@sheltermanager.com>
2026-03-02 11:49:56 +0000

    Add medical type for grooming #2017 #1956

--- src/asm3/dbupdates/35115.py
+++ src/asm3/dbupdates/35115.py
@@ -3,6 +3,7 @@ from asm3.dbupdate import execute
 dbo = dbo
 l = dbo.locale
 
-## Add fluids to lksmedicaltype
+# Add fluids and grooming to lksmedicaltype
 
 execute(dbo, "INSERT INTO lksmedicaltype (ID, MedicalTypeName, Description, ForceSingleUse, IsRetired) VALUES (?, ?, ?, ?, ?)", [ 30, _("Fluids", l), "", 0, 0 ])
+execute(dbo, "INSERT INTO lksmedicaltype (ID, MedicalTypeName, Description, ForceSingleUse, IsRetired) VALUES (?, ?, ?, ?, ?)", [ 31, _("Grooming", l), "", 0, 0 ])
