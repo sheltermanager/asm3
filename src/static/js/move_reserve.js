b6c07b8ac68889cc49bc073db4b2fafd62996885
Adam Spencer <adamdspencer77@gmail.com>
2026-05-06 14:22:39 +0100

    More tidying. #2069

--- src/static/js/move_reserve.js
+++ src/static/js/move_reserve.js
@@ -86,7 +86,6 @@ $(function() {
             $("#person").on("change", async function(event, rec) {
                 let response = await edit_header.person_with_adoption_warnings(rec.ID);
                 let p = jQuery.parseJSON(response)[0];
-                console.log("AJAX executed");
                 $("#ownerwarn").hide();
          
                 // Default giftaid if the person is registered
