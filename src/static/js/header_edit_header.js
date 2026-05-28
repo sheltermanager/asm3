c0d37880d0c2cbebd48cf8bad71b5ed06407e8fb
Adam Spencer <adamdspencer77@gmail.com>
2026-05-28 15:05:42 +0100

    Tidied after feedback. Removed some unneccessary bold tags. #63

--- src/static/js/header_edit_header.js
+++ src/static/js/header_edit_header.js
@@ -603,12 +603,13 @@ edit_header = {
             fostershtml = '<tr><td>' + _("Active Fosters") + ':</td>';
         }
         if (fosters.length > 5) {
-            fostershtml += '<td><b><a href="animal?id=' + fosters[0].ANIMALID + '">' + fosters[0].ANIMALNAME + '</a> <a href="person_movements?id=' + controller.person.ID + '">' + _("plus {0} more...").replace("{0}",  (fosters.length - 1)) + '</a></b></td></tr>';
+            // fostershtml += '<td><b><a href="animal?id=' + fosters[0].ANIMALID + '">' + fosters[0].ANIMALNAME + '</a> <a href="person_movements?id=' + controller.person.ID + '">' + _("plus {0} more...").replace("{0}",  (fosters.length - 1)) + '</a></b></td></tr>';
+            fostershtml += '<td><a href="person_movements?id=' + controller.person.ID + '">' + _("{0} plus {1} more...").replace("{0}", fosters[0].ANIMALNAME).replace("{1}",  (fosters.length - 1)) + '</a></td></tr>';
         } else {
             fostershtml += '<td>';
             let fosterlinks = [];
             $.each(fosters, function(i, v) {
-                fosterlinks.push('<b><a href="animal?id=' + v.ANIMALID + '">' + v.ANIMALNAME + '</a></b>');
+                fosterlinks.push('<a href="animal?id=' + v.ANIMALID + '">' + v.ANIMALNAME + '</a>');
             });
             fostershtml += fosterlinks.join(", ");
             fostershtml += '</td></tr>';
@@ -617,12 +618,13 @@ edit_header = {
             ownedanimalshtml = '<tr><td>' + _("Owned Animals") + ':</td>';
         }
         if (ownedanimals.length > 5) {
-            ownedanimalshtml += '<td><b><a href="animal?id=' + ownedanimals[0].ANIMALID + '">' + ownedanimals[0].ANIMALNAME + '</a> <a href="person_movements?id=' + controller.person.ID + '">' + _("plus {0} more...").replace("{0}",  (ownedanimals.length - 1)) + '</a></b></td></tr>';
+            // ownedanimalshtml += '<td><b><a href="animal?id=' + ownedanimals[0].ANIMALID + '">' + ownedanimals[0].ANIMALNAME + '</a> <a href="person_movements?id=' + controller.person.ID + '">' + _("plus {0} more...").replace("{0}",  (ownedanimals.length - 1)) + '</a></b></td></tr>';
+            ownedanimalshtml += '<td><a href="person_movements?id=' + controller.person.ID + '">' + _("{0} plus {1} more...").replace("{0}", ownedanimals[0].ANIMALNAME).replace("{1}",  (ownedanimals.length - 1)) + '</a></td></tr>';
         } else {
             ownedanimalshtml += '<td>';
             let ownedanimallinks = [];
             $.each(ownedanimals, function(i, v) {
-                ownedanimallinks.push('<b><a href="animal?id=' + v.ANIMALID + '">' + v.ANIMALNAME + '</a></b>');
+                ownedanimallinks.push('<a href="animal?id=' + v.ANIMALID + '">' + v.ANIMALNAME + '</a>');
             });
             ownedanimalshtml += ownedanimallinks.join(", ");
             ownedanimalshtml += '</td></tr>';
