3de0f8279a0d3b3532a46bf6d01901458a9df40d
Adam Spencer <adamdspencer77@gmail.com>
2026-06-23 10:05:10 +0100

    Debugging on dev server. #2097

--- src/static/js/mapview.js
+++ src/static/js/mapview.js
@@ -112,7 +112,29 @@ $(function() {
             }
         },
 
+        _check_leaflet_loaded: function() {
+            try {
+                console.log(L);
+                return true
+            } catch(error) {
+                console.log("Leaflet not available");
+                return false
+            }
+        },
+
         delay: async function() {
+            // window.setTimeout(async function() {
+            //     console.log("Checking leaflet availability");
+            //     let leafletloaded = await mapview._check_leaflet_loaded();
+            //     if (leafletloaded) {
+            //         mapping.draw_map("embeddedmap", 10, false, []);
+            //         mapview.update_markers_from_checkboxes();
+            //     } else {
+            //         console.log("Leaflet not available");
+            //         mapview.delay();
+            //     }
+            // }, 3000);
+
             await mapping.draw_map("embeddedmap", 10, false, []);
             mapview.update_markers_from_checkboxes();
         },
