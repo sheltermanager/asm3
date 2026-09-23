70a17ff7e6e665e807adccd13b403b3d16ae3e74
Adam Spencer <adamdspencer77@gmail.com>
2026-09-23 11:31:10 +0100

    Tweaked after feedback. #2130

--- src/asm3/animal.py
+++ src/asm3/animal.py
@@ -1336,10 +1336,13 @@ def get_animal_find_advanced(dbo: Database, criteria: dict, limit: int = 0, lf: 
     if post["sheltercode"] != "":
         ilike1 = dbo.sql_ilike("a.ShelterCode", "?")
         ilike2 = dbo.sql_ilike("ShelterCode", "?")
-        ilike3 = dbo.sql_ilike("a.ShortCode", "?")
-        ilike4 = dbo.sql_ilike("ShortCode", "?")
-        ss.ands.append(f"( ( ({ilike1} OR EXISTS (SELECT ShelterCode FROM animalentry WHERE {ilike2} AND AnimalID = a.ID)) ) OR ( ({ilike3} OR EXISTS (SELECT ShortCode FROM animalentry WHERE {ilike4} AND AnimalID = a.ID)) ) )")
-        for a in range(0, 4): ss.values.append("%%%s%%" % post["sheltercode"].lower() )
+        ss.ands.append(f"( ( ({ilike1} OR EXISTS (SELECT ShelterCode FROM animalentry WHERE {ilike2} AND AnimalID = a.ID)) ) OR ( (LOWER(a.ShortCode) = ? OR EXISTS (SELECT ShortCode FROM animalentry WHERE LOWER(ShortCode) = ? AND AnimalID = a.ID)) ) )")
+        ss.values += [
+            "%%%s%%" % post["sheltercode"].lower(),
+            "%%%s%%" % post["sheltercode"].lower(),
+            post["sheltercode"].lower(),
+            post["sheltercode"].lower()
+        ]
 
     if post["insuranceno"] != "":
         ilike = dbo.sql_ilike("InsuranceNumber", "?")
